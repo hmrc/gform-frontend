762d8e585cc8d75ffd5141aef6f306bdbd6f0535
Harrison Baxter <harrison.baxter@digital.hmrc.gov.uk>
2017-01-13 17:11:02 +0000

    new environmental bodies can now be added to the form using a javascript button

---
+++ public/javascripts/addInput.js
@@ -0,0 +1,25 @@
+/**
+ * Created by harrison on 13/01/17.
+ */
+
+var counter = 1;
+var limit = 22;
+function addInput(divName){
+    if (counter == limit)  {
+        alert("You have reached the limit of adding " + counter + " inputs");
+    }
+    else {
+        var newdiv = document.createElement('div' + counter);
+        newdiv.innerHTML ='<div style="float: left;" id="name_'+ counter +'"> <dl class=" " id="environmentalBody1_'+ counter +'_bodyName_field"><dl class=" " id="environmentalBody1_'+ counter +'_bodyName_field"> <dt><label for="environmentalBody1_'+ counter +'_bodyName">Name</label></dt> <dd> <input type="text" id="environmentalBody1_'+ counter +'_bodyName" name="environmentalBody1['+ counter +'].bodyName" value="" inputdivclass="col-sm-8" placeholder="" class="form-field-group"> </dd> </dl></div> ' +
+            '              <div style="float: right;" id="amount_'+ counter +'"> <dl class=" " id="environmentalBody1_'+ counter +'_amount_field"> <dt><label for="environmentalBody1_'+ counter +'_amount">Amount</label></dt> <dd> <input type="text" id="environmentalBody1_'+ counter +'_amount" name="environmentalBody1['+ counter +'].amount" value="" inputdivclass="col-sm-8" placeholder="" class="form-field-group"> </dd> </dl> </div>';
+        document.getElementById("newField").appendChild(newdiv);
+        counter++;
+    }
+}
+
+function removeField(divName) {
+    var element = document.getElementById('name_'+ counter +''); // notice the change
+    element.parentNode.removeChild(element);
+    counter --;
+
+}
