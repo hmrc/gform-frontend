6509ff4ab8cdac8031ab834edd83567802e45167
Pawel Panasewicz <pawel.panasewicz@digital.hmrc.gov.uk>
2017-06-30 09:34:16 +0100

    appconfig, wshttp and auditing modules

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -24,7 +24,9 @@ object FrontendBuild extends Build with MicroService {
     "org.julienrf" %% "play-json-derived-codecs" % "3.3",
     "uk.gov.hmrc" %% "play-ui" % "5.3.0",
     "org.typelevel" %% "cats" % "0.9.0",
-    "org.jetbrains" % "markdown" % "0.1.25"
+    "org.jetbrains" % "markdown" % "0.1.25",
+    "com.chuusai" %% "shapeless" % "2.3.2",
+    "com.github.pureconfig" %% "pureconfig" % "0.7.2"
 
   )
 
