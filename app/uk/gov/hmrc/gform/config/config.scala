6b4243131f07cc8dc9b9ac6649eb1d30c786a6fd
Pawel Panasewicz <pawel.panasewicz@digital.hmrc.gov.uk>
2017-07-13 15:10:39 +0100

    pass redirect-success-url parameters to file upload action

--- app/uk/gov/hmrc/gform/config/config.scala
+++ app/uk/gov/hmrc/gform/config/config.scala
@@ -34,7 +34,6 @@ class ConfigModule {
 
 case class AppConfig(
   appName: String,
-  fileUploadRedirect: String, //TODO: we don't need it
   `google-analytics`: GoogleAnalytics,
   `government-gateway-sign-in-url`: String,
   `gform-frontend-base-url`: String
