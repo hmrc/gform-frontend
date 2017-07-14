c961bcc1ae9f9bf872e9f7a08c1881b18a598d03
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-07-14 11:33:19 +0100

    readding version back into formData

--- app/uk/gov/hmrc/gform/gformbackend/model/FormData.scala
+++ app/uk/gov/hmrc/gform/gformbackend/model/FormData.scala
@@ -19,7 +19,7 @@ package uk.gov.hmrc.gform.gformbackend.model
 import play.api.libs.json.Json
 import uk.gov.hmrc.gform.models.UserId
 
-case class FormData(userId: UserId, formTypeId: FormTypeId, characterSet: String, fields: Seq[FormField])
+case class FormData(userId: UserId, formTypeId: FormTypeId, characterSet: String, fields: Seq[FormField], version: Version = Version("0.3.0"))
 
 object FormData {
 
