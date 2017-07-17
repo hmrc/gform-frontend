a74808dcef3119e69fe6c5b54f8290cb6ebcd25e
Pawel Panasewicz <pawel.panasewicz@digital.hmrc.gov.uk>
2017-07-17 14:48:32 +0100

    there is always some formId

--- app/uk/gov/hmrc/gform/models/Page.scala
+++ app/uk/gov/hmrc/gform/models/Page.scala
@@ -53,7 +53,7 @@ case class Page(formId: FormId, sectionNumber: SectionNumber, formTemplate: Form
   def pageForRender(fieldData: Map[FieldId, Seq[String]], f: Option[FieldValue => Option[FormFieldValidationResult]])(implicit authContext: AuthContext, hc: HeaderCarrier): Future[PageForRender] =
     PageForRender(formId, sectionNumber, fieldData, formTemplate, f, repeatService, envelope, envelopeId)
 
-  def renderPage(fieldData: Map[FieldId, Seq[String]], formId: Option[FormId], f: Option[FieldValue => Option[FormFieldValidationResult]])(implicit request: Request[_], messages: Messages, authContext: AuthContext, hc: HeaderCarrier): Future[Result] = {
+  def renderPage(fieldData: Map[FieldId, Seq[String]], formId: FormId, f: Option[FieldValue => Option[FormFieldValidationResult]])(implicit request: Request[_], messages: Messages, authContext: AuthContext, hc: HeaderCarrier): Future[Result] = {
     pageForRender(fieldData, f).map(page => Ok(uk.gov.hmrc.gform.views.html.form(formTemplate, page, formId)))
   }
 }
