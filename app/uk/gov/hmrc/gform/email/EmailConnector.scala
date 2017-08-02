9594e21f703f4d4c43f68e7adb692a2e522e9902
Harrison Baxter <harrison.baxter@digital.hmrc.gov.uk>
2017-08-02 15:09:36 +0100

    Implemented the email reciept but can not test as there is no input for the email address. And because QA is broken and staging and dev don't send emails

---
+++ app/uk/gov/hmrc/gform/email/EmailConnector.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gform.email
+
+import uk.gov.hmrc.gform.wshttp.WSHttp
+import uk.gov.hmrc.play.http._
+
+import scala.concurrent.{ ExecutionContext, Future }
+
+class EmailConnector(wsHttp: WSHttp, baseUrl: String) {
+
+  def sendEmail(emailTemplate: EmailTemplate)(implicit headerCarrier: HeaderCarrier, ec: ExecutionContext): Future[Boolean] = {
+    wsHttp.POST[EmailTemplate, HttpResponse](baseUrl + "hmrc/email", emailTemplate).map { response =>
+      response.status == 202
+    }
+  }
+}
