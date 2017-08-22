7dfe421fbe93e31ec8fd088f8d4fe629f6b6f2f5
pawel.panasewicz <pawel.panasewicz@gmail.com>
2017-08-22 09:55:37 +0100

    using authentication.async which takes optional formId which fixes build

--- test/uk/gov/hmrc/gform/controllers/NavigatorSpec.scala
+++ test/uk/gov/hmrc/gform/controllers/NavigatorSpec.scala
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.gform.controllers
 
 import uk.gov.hmrc.gform.Spec
 import uk.gov.hmrc.gform.controllers.Navigator
 import uk.gov.hmrc.gform.sharedmodel.ExampleData
-import uk.gov.hmrc.gform.sharedmodel.formtemplate.{IncludeIf, IsFalse}
+import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ IncludeIf, IsFalse }
 
 class SaveSpec extends Spec {
 
@@ -61,10 +77,9 @@ class SaveSpec extends Spec {
   behavior of "new Navigator"
 
   it should "throw exception if section numbers are out of bounds" in {
-    an[IllegalArgumentException] shouldBe thrownBy (new Navigator(`sectionNumber-1`, allSections, rawDataFromBrowser))
-    an[IllegalArgumentException] shouldBe thrownBy (new Navigator(sectionNumber3, allSections, rawDataFromBrowser))
+    an[IllegalArgumentException] shouldBe thrownBy(new Navigator(`sectionNumber-1`, allSections, rawDataFromBrowser))
+    an[IllegalArgumentException] shouldBe thrownBy(new Navigator(sectionNumber3, allSections, rawDataFromBrowser))
   }
-
 
   trait Fixture extends ExampleData {
     override def allSections = List(
