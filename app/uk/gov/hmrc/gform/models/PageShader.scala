036e09152cf4d6eb61f4f186bc25f86b1aca10f0
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-08-23 17:39:42 +0100

    fixing failing tests

--- app/uk/gov/hmrc/gform/models/PageShader.scala
+++ app/uk/gov/hmrc/gform/models/PageShader.scala
@@ -64,8 +64,8 @@ class PageShader(
   }
 
   private def createJavascript(fieldList: List[FieldValue], atomicFields: List[FieldValue]) = {
-    val groups: List[(FieldId, Group)] = fieldList.map(fv => (fv.id, fv.`type`)).collect{
-      case (fieldId, group : Group) => (fieldId, group)
+    val groups: List[(FieldId, Group)] = fieldList.map(fv => (fv.id, fv.`type`)).collect {
+      case (fieldId, group: Group) => (fieldId, group)
     }
     groups.map(x => collapsingGroupJavascript(x._1, x._2)).mkString(";\n") + fieldJavascript(atomicFields)
   }
