d63e8cca91f660c2f9058cdd4cc87b6a742a0ef7
daniel.connelly <daniel.connelly@digital.hmrc.gov.uk>
2017-10-11 16:28:36 +0100

    fixing the eeitt prepopulation as it was hitting the wrong url and using the wrong regimeId

--- app/uk/gov/hmrc/gform/connectors/EeittConnectors.scala
+++ app/uk/gov/hmrc/gform/connectors/EeittConnectors.scala
@@ -38,7 +38,7 @@ class EeittConnector(baseUrl: String, wSHttp: WSHttp) {
   def isAllowed(groupId: String, regimeId: RegimeId, affinityGroup: AffinityGroup)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Verification] =
     wSHttp.GET[Verification](baseUrl + s"/group-id/${encode(groupId)}/regime/${regimeId.value}/affinityGroup/${encode(affinityGroup.toString)}/verification")
 
-  def prepopulationBusinessUser(groupId: GroupId, regimeId: FormTemplateId)(implicit hc: HeaderCarrier): Future[BusinessUser] = {
+  def prepopulationBusinessUser(groupId: GroupId, regimeId: RegimeId)(implicit hc: HeaderCarrier): Future[BusinessUser] = {
     wSHttp.GET[BusinessUser](baseUrl + s"/group-id/${groupId.value}/regime/${regimeId.value}/prepopulation")
   }
 
