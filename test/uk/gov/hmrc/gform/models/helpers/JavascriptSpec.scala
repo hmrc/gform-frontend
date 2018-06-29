3bcf1c0ad5c71b15ece8676454031bbc06c9e586
Dave English <dave@lutnos.com>
2018-06-29 10:22:11 +0100

    Suppress Javascript calculations which are purely static (#429)
    * Suppress Javascript calculations which are purely static, i.e. dependent only on fields from other sections
    
    Interim
    
    Compiles, needs tests
    
    Add unit tests
    
    * Correct an indent and licence text on new file

---
+++ test/uk/gov/hmrc/gform/models/helpers/JavascriptSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gform.models.helpers
+
+import cats.implicits._
+import uk.gov.hmrc.gform.Spec
+import uk.gov.hmrc.gform.sharedmodel.formtemplate._
+
+import scala.concurrent.Future
+
+class JavascriptSpec extends Spec {
+
+  private val c = Constant("")
+
+  def formComponent(id: String, value: Expr = c) =
+    FormComponent(FormComponentId(id), Text(AnyText, value), "", None, None, None, false, true, true, true, false, None)
+
+  private def fieldJavascript(field: FormComponent) = {
+    val fields = List(formComponent("thisSection"), field)
+    Javascript.fieldJavascript(
+      sectionFields = fields,
+      allFields = formComponent("otherSection") :: fields,
+      groupList = List[List[List[FormComponent]]]().pure[Future])
+  }
+
+  "if calculation references only a constant" should "not generate Javascript for the static calculation" in {
+    val result = fieldJavascript(formComponent("staticExpr"))
+    result.futureValue should not include ("addstaticExpr")
+  }
+
+  "if calculation references only a field in this section" should "not generate Javascript for the static calculation" in {
+    val result = fieldJavascript(formComponent("staticExpr", FormCtx("thisSection")))
+    result.futureValue should not include ("addstaticExpr")
+  }
+
+  "if calculation references only a group in this section" should "generate Javascript for the dynamic calculation" in {
+    val result = fieldJavascript(formComponent("dynamicExpr", Sum(FormCtx("thisSection"))))
+    result.futureValue should include("sumthisSection")
+  }
+
+  "if calculation adds a field in this section" should "generate Javascript for the dynamic calculation" in {
+    val result = fieldJavascript(formComponent("dynamicExpr", Add(FormCtx("thisSection"), c)))
+    result.futureValue should include("adddynamicExpr")
+  }
+
+  "if calculation deep inside uses a field in this section" should "generate Javascript for the dynamic calculation" in {
+    val result = fieldJavascript(
+      formComponent(
+        "dynamicExpr",
+        Add(c, Add(Subtraction(c, Subtraction(Multiply(c, Multiply(FormCtx("thisSection"), c)), c)), c))))
+    result.futureValue should include("adddynamicExpr")
+  }
+
+}
