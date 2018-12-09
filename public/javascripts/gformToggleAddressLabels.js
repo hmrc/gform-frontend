558eb6805f7b416c8578fa17dbc0606b5ccb9f60
Simon Bailey <simon@pelli.co.uk>
2018-12-09 21:59:26 +0000

    GFC-620 refactor gform JS

---
+++ public/javascripts/gformToggleAddressLabels.js
@@ -0,0 +1,44 @@
+;(function (global) {
+    'use strict'
+  
+    var $ = global.jQuery
+    var GOVUK = global.GOVUK || {}
+  
+    function GformToggleAddressLabels () {
+      var self = this
+
+      function showLabel($label) {
+        $label.removeClass('js-hidden').attr('aria-hidden', false)
+      }
+    
+      function hideLabel($label) {
+        $label.addClass('js-hidden').attr('aria-hidden', true)
+      }
+    
+      function toggleAddressLabels(e) {
+        var choice = $(e.target).attr('data-address-choice');
+        $('span[data-address-label]').each(function (i, label) {
+          $(label).attr('data-address-label') === choice ? showLabel($(label)) : hideLabel($(label))
+        })
+      }
+  
+      // Set up event handlers
+      function init () {
+        $('input[data-address-choice]').on('click', toggleAddressLabels);
+      }
+      
+      self.GformToggleAddressLabels = function () {
+        init()
+      }
+  
+    }
+  
+    GformToggleAddressLabels.prototype.init = function () {
+      this.GformToggleAddressLabels()
+    }
+  
+    GOVUK.GformToggleAddressLabels = GformToggleAddressLabels
+    global.GOVUK = GOVUK
+  })(window)
+
+  
