236fea754ab490faf2e66b2e0ce01223ea7f2390
Josef Vlach <serg3j@gmail.com>
2019-01-03 10:30:11 +0000

    Fix NPE compilation error

--- test/uk/gov/hmrc/gform/Spec.scala
+++ test/uk/gov/hmrc/gform/Spec.scala
@@ -19,10 +19,8 @@ package uk.gov.hmrc.gform
 import cats.scalatest.EitherMatchers
 import org.scalatest._
 import org.scalatest.concurrent.{ Eventually, ScalaFutures }
-import org.scalatest.matchers.{ BeMatcher, MatchResult, Matcher }
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.time.{ Millis, Span }
-import play.api.libs.json.JsResult
 import uk.gov.hmrc.gform.sharedmodel.ExampleData
 
 import scala.concurrent.ExecutionContext
@@ -36,38 +34,4 @@ trait Spec
     PatienceConfig(timeout = scaled(Span(1000, Millis)), interval = scaled(Span(15, Millis)))
 
   implicit lazy val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
-}
-
-trait JsResultMatcher { self: Spec =>
-
-  def beJsSuccess[E](element: E): Matcher[JsResult[E]] = new BeJsSuccess[E](element)
-
-  def jsError[E]: BeMatcher[JsResult[E]] = new IsJsErrorMatcher[E]
-
-  def beJsError[E](message: String): Matcher[JsResult[E]] = new BeJsError[E](message)
-
-  final private class BeJsSuccess[E](element: E) extends Matcher[JsResult[E]] {
-    def apply(jsResult: JsResult[E]): MatchResult =
-      MatchResult(
-        jsResult.fold(_ => false, _ == element),
-        s"'$jsResult' did not contain an element matching '$element'.",
-        s"'$jsResult' contained an element matching '$element', but should not have."
-      )
-  }
-
-  final private class BeJsError[E](message: String) extends Matcher[JsResult[E]] {
-    def apply(jsResult: JsResult[E]): MatchResult =
-      MatchResult(
-        jsResult.fold(_.exists(_._2.exists(_.messages.exists(_ == message))), _ => false),
-        s"'$jsResult' was not an error with a message matching '$message'.",
-        s"'$jsResult' contained an error with a message matching '$message', but should not have."
-      )
-  }
-  final private class IsJsErrorMatcher[E] extends BeMatcher[JsResult[E]] {
-    def apply(jsResult: JsResult[E]): MatchResult =
-      MatchResult(
-        jsResult.isError,
-        s"'$jsResult' was not an JsError, but should have been.",
-        s"'$jsResult' was an JsError, but should *NOT* have been.")
-  }
 }
