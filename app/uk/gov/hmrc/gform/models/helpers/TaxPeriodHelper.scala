e6f079e29f862668eb7a03c717ed846c87d8e05c
Kyle Chown <kyle.chowns@digital.hmrc.gov.uk>
2019-01-15 10:54:24 +0000

    Date correctly formatted in summary. Created two possible methods

---
+++ app/uk/gov/hmrc/gform/models/helpers/TaxPeriodHelper.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gform.models.helpers
+import uk.gov.hmrc.gform.validation.{ ComponentField, FormFieldValidationResult }
+import uk.gov.hmrc.gform.views.summary.TextFormatter
+
+object TaxPeriodHelper {
+
+  def formatOutput(valResult: Option[FormFieldValidationResult]) = {
+    val mapOfResults = valResult.get match { case ComponentField(a, b) => b }
+    mapOfResults.keySet.toString.split('(')(2).split(',')(0)
+  }
+
+  def formatOutput2(valResult: Option[FormFieldValidationResult]) = {
+    val mapOfResults = valResult.get match { case ComponentField(a, b) => b }
+    TextFormatter.formatText(Some(mapOfResults.values.toList(0))).drop(1)
+  }
+
+}
