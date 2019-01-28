071fc9b02f16b7fe6ef2aa31531574fa35d4c872
Kyle Chown <45938554+kylemchownhmrc@users.noreply.github.com>
2019-01-28 16:09:40 +0000

    Fixed various match statements

--- app/uk/gov/hmrc/gform/models/helpers/TaxPeriodHelper.scala
+++ app/uk/gov/hmrc/gform/models/helpers/TaxPeriodHelper.scala
@@ -18,15 +18,19 @@ package uk.gov.hmrc.gform.models.helpers
 import java.text.SimpleDateFormat
 import java.util.Date
 
-import uk.gov.hmrc.gform.validation.{ ComponentField, FormFieldValidationResult }
+import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ FileUpload, FormComponent, FormComponentId }
+import uk.gov.hmrc.gform.validation.{ ComponentField, FieldOk, FormFieldValidationResult }
 import uk.gov.hmrc.gform.views.summary.TextFormatter
 
 object TaxPeriodHelper {
 
+  val dummyFormComponent =
+    FormComponent(new FormComponentId(""), FileUpload(), "", None, None, None, false, false, false, false, false, None)
+
   def formatTaxPeriodOutput(valResult: Option[FormFieldValidationResult]) = {
     val a = valResult match {
       case Some(x) => x
-      case _       => ""
+      case _       => FieldOk(dummyFormComponent, "")
     }
     a match {
       case ComponentField(a, b) => TextFormatter.formatText(b.values.headOption).drop(1)
