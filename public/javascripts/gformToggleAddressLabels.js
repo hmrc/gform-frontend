fb58f8a23948d77cdce9aaad9d8f8c4f64605a89
Rob Bond <46452743+robbond@users.noreply.github.com>
2019-02-01 15:49:48 +0000

    Rdb/gfc 748/address component not clear postcode when toggling (#599)
    GFC-748: address toggling now clears field content

--- public/javascripts/gformToggleAddressLabels.js
+++ public/javascripts/gformToggleAddressLabels.js
@@ -13,15 +13,19 @@
     
       function hideLabel($label) {
         $label.addClass('js-hidden').attr('aria-hidden', true)
+        $('#' + $label.parent().attr('for')).val('')
       }
     
       function toggleAddressLabels(e) {
+
         var choice = $(e.target).attr('data-address-choice');
-        $('span[data-address-label]').each(function (i, label) {
+        var scopeVar = $(e.target).closest('[id$="-fieldset"]')
+
+        scopeVar.find('span[data-address-label]').each(function (i, label) {
           $(label).attr('data-address-label') === choice ? showLabel($(label)) : hideLabel($(label))
         })
       }
-  
+
       // Set up event handlers
       function init () {
         $('input[data-address-choice]').on('click', toggleAddressLabels);
