0bba8319c9a5037ec2702cde096a055e2e6b383b
rob bond <>
2019-02-01 13:40:28 +0000

    Adjusted toggle function
    Adjusted toggle function to empty the current address fields before displaying the new ones when toggling between uk nd international addresses

--- public/javascripts/gformToggleAddressLabels.js
+++ public/javascripts/gformToggleAddressLabels.js
@@ -12,16 +12,24 @@
       }
     
       function hideLabel($label) {
+        $label.find('input').val('')
+        $('#' + $($label).parent().attr('for')).val('')
         $label.addClass('js-hidden').attr('aria-hidden', true)
       }
     
       function toggleAddressLabels(e) {
+
+        var addressType = $(e.target).attr('aria-controls');
+        $("div[id*='address-']").each(function (i, whichDiv) {
+          $(whichDiv).attr('id') === addressType ? showLabel($(whichDiv)) : hideLabel($(whichDiv))
+        })
+
         var choice = $(e.target).attr('data-address-choice');
         $('span[data-address-label]').each(function (i, label) {
           $(label).attr('data-address-label') === choice ? showLabel($(label)) : hideLabel($(label))
         })
       }
-  
+
       // Set up event handlers
       function init () {
         $('input[data-address-choice]').on('click', toggleAddressLabels);
