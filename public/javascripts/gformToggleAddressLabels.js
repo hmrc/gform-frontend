206a6e6d7b2b25e085bd1444451c17a61a9edb19
rob bond <>
2019-02-01 15:28:20 +0000

    Adjusted toggle for multiple address
    adjusted toggle for multiple address fieldsets on page scenario

--- public/javascripts/gformToggleAddressLabels.js
+++ public/javascripts/gformToggleAddressLabels.js
@@ -12,20 +12,16 @@
       }
     
       function hideLabel($label) {
-        $label.find('input').val('')
-        $('#' + $($label).parent().attr('for')).val('')
         $label.addClass('js-hidden').attr('aria-hidden', true)
+        $('#' + $label.parent().attr('for')).val('')
       }
     
       function toggleAddressLabels(e) {
 
-        var addressType = $(e.target).attr('aria-controls');
-        $("div[id*='address-']").each(function (i, whichDiv) {
-          $(whichDiv).attr('id') === addressType ? showLabel($(whichDiv)) : hideLabel($(whichDiv))
-        })
-
         var choice = $(e.target).attr('data-address-choice');
-        $('span[data-address-label]').each(function (i, label) {
+        var scopeVar = $(e.target).closest('[id$="-fieldset"]')
+
+        scopeVar.find('span[data-address-label]').each(function (i, label) {
           $(label).attr('data-address-label') === choice ? showLabel($(label)) : hideLabel($(label))
         })
       }
