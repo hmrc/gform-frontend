b4227191957132952513ca1cb44f7cf8f53de1cd
Pas <pasquale.gatto@digital.hmrc.gov.uk>
2019-02-14 09:36:50 +0000

    Resolve conflicts

--- test/uk/gov/hmrc/gform/graph/processor/UserCtxEvaluatorProcessorTest.scala
+++ test/uk/gov/hmrc/gform/graph/processor/UserCtxEvaluatorProcessorTest.scala
@@ -18,17 +18,18 @@ package uk.gov.hmrc.gform.graph.processor
 
 import cats.Id
 import uk.gov.hmrc.auth.core.retrieve.OneTimeLogin
-import uk.gov.hmrc.auth.core.{Enrolment, EnrolmentIdentifier, Enrolments, AffinityGroup => CoreAffinityGroup}
+import uk.gov.hmrc.auth.core.{ Enrolment, EnrolmentIdentifier, Enrolments, AffinityGroup => CoreAffinityGroup }
 import uk.gov.hmrc.gform.Spec
-import uk.gov.hmrc.gform.auth.models.{AnonymousRetrievals, AuthenticatedRetrievals}
+import uk.gov.hmrc.gform.auth.models.{ AnonymousRetrievals, AuthenticatedRetrievals }
 import uk.gov.hmrc.gform.graph.NonConvertible
-import uk.gov.hmrc.gform.sharedmodel.formtemplate.{AllowAnyAgentAffinityUser, EnrolledIdentifier, EnrolmentAuth, HmrcAgentWithEnrolmentModule, HmrcEnrolmentModule, HmrcSimpleModule, Never, ServiceId, UserCtx, AffinityGroup => FTAffinityGroup}
+import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ AllowAnyAgentAffinityUser, EnrolledIdentifier, EnrolmentAuth, HmrcAgentWithEnrolmentModule, HmrcEnrolmentModule, HmrcSimpleModule, Never, ServiceId, UserCtx, AffinityGroup => FTAffinityGroup }
 import uk.gov.hmrc.http.logging.SessionId
 
 class UserCtxEvaluatorProcessorTest extends Spec {
 
   forAll(userCtxTable) { (enr, field, serviceId, authConfig, expected) =>
-    it should s"evaluate a user ctx with $enr, $field, $serviceId and $authConfig return $expected" in new UserCtxEvaluatorProcessor[Id] {
+    it should s"evaluate a user ctx with $enr, $field, $serviceId and $authConfig return $expected" in new UserCtxEvaluatorProcessor[
+      Id] {
       val retrievals: AuthenticatedRetrievals = materialisedRetrievalsAgent.copy(enrolments = Enrolments(Set(enr)))
 
       processEvaluation(retrievals, UserCtx(field), authConfig) should be(expected)
@@ -36,18 +37,48 @@ class UserCtxEvaluatorProcessorTest extends Spec {
   }
 
   it should "return an empty string when authModule in authConfig is set anonymous" in new UserCtxEvaluatorProcessor[Id] {
-    processEvaluation(AnonymousRetrievals(SessionId("id")), UserCtx(EnrolledIdentifier), authConfig) should be(NonConvertible(""))
+    processEvaluation(AnonymousRetrievals(SessionId("id")), UserCtx(EnrolledIdentifier), authConfig) should be(
+      NonConvertible(""))
   }
 
   lazy val userCtxTable = Table(
     ("enr", "userField", "serviceId", "authConfig", "NoncovertibleResult"),
-    (irctEnrolment, EnrolledIdentifier, ServiceId("IR-CT"), HmrcEnrolmentModule(EnrolmentAuth(ServiceId("IR-CT"), Never)), NonConvertible("CT value")),
-    (irsaEnrolment, EnrolledIdentifier, ServiceId("IR-SA"), HmrcEnrolmentModule(EnrolmentAuth(ServiceId("IR-SA"), Never)), NonConvertible("SA value")),
-    (irsaEnrolment, EnrolledIdentifier, ServiceId("IR-SA"), HmrcAgentWithEnrolmentModule(AllowAnyAgentAffinityUser,EnrolmentAuth(ServiceId("IR-SA"), Never)), NonConvertible("SA value")),
-    (irctEnrolment, EnrolledIdentifier, ServiceId(""), HmrcEnrolmentModule(EnrolmentAuth(ServiceId(""), Never)), NonConvertible("")),
-    (irctEnrolment, FTAffinityGroup, ServiceId(""), HmrcSimpleModule, NonConvertible("agent")))
+    (
+      irctEnrolment,
+      EnrolledIdentifier,
+      ServiceId("IR-CT"),
+      HmrcEnrolmentModule(EnrolmentAuth(ServiceId("IR-CT"), Never)),
+      NonConvertible("CT value")),
+    (
+      irsaEnrolment,
+      EnrolledIdentifier,
+      ServiceId("IR-SA"),
+      HmrcEnrolmentModule(EnrolmentAuth(ServiceId("IR-SA"), Never)),
+      NonConvertible("SA value")),
+    (
+      irsaEnrolment,
+      EnrolledIdentifier,
+      ServiceId("IR-SA"),
+      HmrcAgentWithEnrolmentModule(AllowAnyAgentAffinityUser, EnrolmentAuth(ServiceId("IR-SA"), Never)),
+      NonConvertible("SA value")),
+    (
+      irctEnrolment,
+      EnrolledIdentifier,
+      ServiceId(""),
+      HmrcEnrolmentModule(EnrolmentAuth(ServiceId(""), Never)),
+      NonConvertible("")),
+    (irctEnrolment, FTAffinityGroup, ServiceId(""), HmrcSimpleModule, NonConvertible("agent"))
+  )
 
-  lazy val materialisedRetrievalsAgent = AuthenticatedRetrievals(OneTimeLogin, Enrolments(Set(irsaEnrolment)), Some(CoreAffinityGroup.Agent), None, None, userDetails, None, None)
+  lazy val materialisedRetrievalsAgent = AuthenticatedRetrievals(
+    OneTimeLogin,
+    Enrolments(Set(irsaEnrolment)),
+    Some(CoreAffinityGroup.Agent),
+    None,
+    None,
+    userDetails,
+    None,
+    None)
 
   lazy val irsaEnrolment = Enrolment("IR-SA").copy(identifiers = Seq(EnrolmentIdentifier("UTR", "SA value")))
   lazy val irctEnrolment = Enrolment("IR-CT").copy(identifiers = Seq(EnrolmentIdentifier("UTR", "CT value")))
