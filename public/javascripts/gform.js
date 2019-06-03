abd4fe0f16587044c75dce7967c581110e3b7560
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2019-06-03 14:57:50 +0100

    GFC-915: turned off testing timeout vals

--- public/javascripts/gform.js
+++ public/javascripts/gform.js
@@ -30,8 +30,8 @@
 
   if (window.gform && window.gform.config && window.gform.config.timeoutEnabled) {
     GOVUK.gformSessionTimeout({
-      timeout: 30,
-      countdown: 15,
+      timeout: window.gform.config.timeout,
+      countdown: window.gform.config.countdown,
       keep_alive_url: window.gform.config.keep_alive_url,
       logout_url: window.gform.config.logout_url
     })
