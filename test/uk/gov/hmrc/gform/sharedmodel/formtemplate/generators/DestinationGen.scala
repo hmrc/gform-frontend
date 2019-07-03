a3623854fa1898de281635ebd583ab28f219f0a1
Lance Walton <461999+lancewalton@users.noreply.github.com>
2019-07-03 10:45:42 +0100

    Removed now unnecessary OFSTED related stuff

--- test/uk/gov/hmrc/gform/sharedmodel/formtemplate/generators/DestinationGen.scala
+++ test/uk/gov/hmrc/gform/sharedmodel/formtemplate/generators/DestinationGen.scala
@@ -52,33 +52,6 @@ trait DestinationGen {
       failOnError <- PrimitiveGen.booleanGen
     } yield Destination.HandlebarsHttpApi(id, profile, uri, method, payload, payloadType, includeIf, failOnError)
 
-  def reviewingOfstedGen: Gen[Destination.ReviewingOfsted] =
-    for {
-      id          <- destinationIdGen
-      cfid        <- FormComponentGen.formComponentIdGen
-      rtid        <- FormTemplateGen.formTemplateIdGen
-      userId      <- UserIdGen.userIdGen
-      includeIf   <- includeIfGen
-      failOnError <- PrimitiveGen.booleanGen
-    } yield Destination.ReviewingOfsted(id, cfid, rtid, userId, includeIf, failOnError)
-
-  def reviewRejectionGen: Gen[Destination.ReviewRejection] =
-    for {
-      id                       <- destinationIdGen
-      correlationFieldId       <- FormComponentGen.formComponentIdGen
-      reviewFormCommentFieldId <- FormComponentGen.formComponentIdGen
-      includeIf                <- includeIfGen
-      failOnError              <- PrimitiveGen.booleanGen
-    } yield Destination.ReviewRejection(id, correlationFieldId, reviewFormCommentFieldId, includeIf, failOnError)
-
-  def reviewApprovalGen: Gen[Destination.ReviewApproval] =
-    for {
-      id                 <- destinationIdGen
-      correlationFieldId <- FormComponentGen.formComponentIdGen
-      includeIf          <- includeIfGen
-      failOnError        <- PrimitiveGen.booleanGen
-    } yield Destination.ReviewApproval(id, correlationFieldId, includeIf, failOnError)
-
   def compositeGen: Gen[Destination.Composite] =
     for {
       id           <- destinationIdGen
@@ -95,19 +68,13 @@ trait DestinationGen {
     } yield Destination.StateTransition(id, requiredState, includeIf, failOnError)
 
   def singularDestinationGen: Gen[Destination] =
-    Gen.oneOf(
-      hmrcDmsGen,
-      handlebarsHttpApiGen,
-      reviewingOfstedGen,
-      reviewRejectionGen,
-      reviewApprovalGen,
-      stateTransitionGen)
+    Gen.oneOf(hmrcDmsGen, handlebarsHttpApiGen, stateTransitionGen)
 
   def destinationGen: Gen[Destination] = Gen.frequency(10 -> singularDestinationGen, 1 -> compositeGen)
 
   def destinationWithFixedIdGen(id: DestinationId): Gen[Destination] = hmrcDmsGen.map(_.copy(id = id))
 
-  def includeIfGen(): Gen[String] = Gen.oneOf(Gen.alphaNumStr, Gen.const("true"), Gen.const("false"))
+  def includeIfGen: Gen[String] = Gen.oneOf(Gen.alphaNumStr, Gen.const("true"), Gen.const("false"))
 }
 
 object DestinationGen extends DestinationGen
