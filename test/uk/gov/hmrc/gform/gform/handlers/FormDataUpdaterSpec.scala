a54c4a0465b493b6697ce66673940031276856c2
Pasquale Gatto <pasquale.gatto@digital.hmrc.gov.uk>
2019-07-17 09:26:57 +0100

    MOR-57 Link individual details on link button request from AWS

---
+++ test/uk/gov/hmrc/gform/gform/handlers/FormDataUpdaterSpec.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.gform.gform.handlers
+
+import uk.gov.hmrc.gform.Spec
+import uk.gov.hmrc.gform.sharedmodel.form._
+import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ FormComponentId, FormTemplateId }
+import uk.gov.hmrc.gform.sharedmodel.{ NotChecked, UserId }
+
+class FormDataUpdaterSpec extends Spec {
+
+  it should s"update FormField in form data" in new FormDataUpdater {
+    val formFields = Seq(
+      FormField(FormComponentId("1"), "one"),
+      FormField(FormComponentId("2"), "two"),
+      FormField(FormComponentId("3"), "three"))
+
+    val existingForm = Form(
+      FormId("666"),
+      EnvelopeId("id1"),
+      UserId("usr"),
+      FormTemplateId("temp"),
+      FormData(formFields),
+      Accepted,
+      VisitIndex(Set.empty),
+      ThirdPartyData(None, NotChecked),
+      None
+    )
+
+    val updatedForm = updateFormField(existingForm, FormField(FormComponentId("2"), "xxx"))
+
+    updatedForm.formData.fields contains Seq(
+      FormField(FormComponentId("1"), "one"),
+      FormField(FormComponentId("2"), "xxx"),
+      FormField(FormComponentId("3"), "three"))
+  }
+}
