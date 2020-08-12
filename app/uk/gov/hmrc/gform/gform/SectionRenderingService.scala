bffd2b85271f51f5d6ae926e025677affc8a990f
Sandip Khedekar <Sandip.Khedekar16@gmail.com>
2020-08-12 17:38:31 +0100

    GFORMS-847 - DisplayHeading fix (#940)

--- app/uk/gov/hmrc/gform/gform/SectionRenderingService.scala
+++ app/uk/gov/hmrc/gform/gform/SectionRenderingService.scala
@@ -1576,9 +1576,9 @@ class SectionRenderingService(frontendAppConfig: FrontendAppConfig, lookupRegist
 object IsNilOrInfoOnly {
   def unapply(xs: List[FormComponent]): Boolean =
     xs match {
-      case Nil                                    => true
-      case IsInformationMessage(_) :: tail        => unapply(tail)
-      case head :: tail if head.onlyShowOnSummary => unapply(tail)
-      case _                                      => false
+      case Nil                                                      => true
+      case IsInformationMessage(_) :: tail                          => unapply(tail)
+      case head :: tail if head.onlyShowOnSummary || !head.editable => unapply(tail)
+      case _                                                        => false
     }
 }
