57a614466715d74fb20d4fa2f066d5b3b48f0dfe
Josef Vlach <38655672+josef-vlach@users.noreply.github.com>
2020-11-30 16:15:39 +0000

    GFORMS-912 - AddToList iteration delete fix - unit test

--- test/uk/gov/hmrc/gform/models/AddToListUtilsSpec.scala
+++ test/uk/gov/hmrc/gform/models/AddToListUtilsSpec.scala
@@ -16,142 +16,199 @@
 
 package uk.gov.hmrc.gform.models
 
-/*
- * import org.scalatest.prop.TableDrivenPropertyChecks.{ Table, forAll }
- * import uk.gov.hmrc.gform.graph.FormTemplateBuilder._
- * import uk.gov.hmrc.gform.sharedmodel.VariadicValue.{ Many, One }
- * import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ BasicText, FormComponent, FormComponentId, Section, Text, Value }
- * import uk.gov.hmrc.gform.sharedmodel.{ NotChecked, Obligations }
- * import uk.gov.hmrc.gform.sharedmodel.form.{ FormDataRecalculated, VisitIndex }
- * import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ AddToListId, FullyExpanded }
- */
-import org.scalatest.{ FlatSpec, Inside, Matchers }
-class AddToListUtilsSpec extends FlatSpec with Matchers with Inside with FormModelSupport {
-  /*   "AddToListUtils.removeRecord" should "remove instance by its index" in {
- *     val data: FormDataRecalculated = mkVariadicFormDataRecalculated(
- *       "regular"   -> One("r"),
- *       "1_a"       -> One("a_1"),
- *       "1_b"       -> One("b_1"),
- *       "1_c"       -> One("c_1"),
- *       "1_e"       -> One("d_1"),
- *       "1_c"       -> One("e_1"),
- *       "2_a"       -> One("a_2"),
- *       "2_b"       -> One("b_2"),
- *       "2_c"       -> One("c_2"),
- *       "2_e"       -> One("d_2"),
- *       "2_c"       -> One("e_2"),
- *       "3_a"       -> One("a_3"),
- *       "3_b"       -> One("b_3"),
- *       "3_c"       -> One("c_3"),
- *       "3_e"       -> One("d_3"),
- *       "3_c"       -> One("e_3"),
- *       "1_ownerFc" -> Many("0" :: Nil),
- *       "2_ownerFc" -> Many("0" :: Nil)
- *     )
- *
- *     val expectedData1: FormDataRecalculated = mkVariadicFormDataRecalculated(
- *       "regular"   -> One("r"),
- *       "1_a"       -> One("a_2"),
- *       "1_b"       -> One("b_2"),
- *       "1_c"       -> One("c_2"),
- *       "1_e"       -> One("d_2"),
- *       "1_c"       -> One("e_2"),
- *       "2_a"       -> One("a_3"),
- *       "2_b"       -> One("b_3"),
- *       "2_c"       -> One("c_3"),
- *       "2_e"       -> One("d_3"),
- *       "2_c"       -> One("e_3"),
- *       "1_ownerFc" -> Many("0" :: Nil)
- *     )
- *
- *     val expectedData2: FormDataRecalculated = mkVariadicFormDataRecalculated(
- *       "regular"   -> One("r"),
- *       "1_a"       -> One("a_1"),
- *       "1_b"       -> One("b_1"),
- *       "1_c"       -> One("c_1"),
- *       "1_e"       -> One("d_1"),
- *       "1_c"       -> One("e_1"),
- *       "2_a"       -> One("a_3"),
- *       "2_b"       -> One("b_3"),
- *       "2_c"       -> One("c_3"),
- *       "2_e"       -> One("d_3"),
- *       "2_c"       -> One("e_3"),
- *       "1_ownerFc" -> Many("0" :: Nil)
- *     )
- *
- *     val expectedData3: FormDataRecalculated = mkVariadicFormDataRecalculated(
- *       "regular"   -> One("r"),
- *       "1_a"       -> One("a_1"),
- *       "1_b"       -> One("b_1"),
- *       "1_c"       -> One("c_1"),
- *       "1_e"       -> One("d_1"),
- *       "1_c"       -> One("e_1"),
- *       "2_a"       -> One("a_2"),
- *       "2_b"       -> One("b_2"),
- *       "2_c"       -> One("c_2"),
- *       "2_e"       -> One("d_2"),
- *       "2_c"       -> One("e_2"),
- *       "1_ownerFc" -> Many("0" :: Nil),
- *       "2_ownerFc" -> Many("0" :: Nil)
- *     )
- *
- *     val sections: List[Section] =
- *       mkSection(mkFormComponent("regular", Text(BasicText, Value)) :: Nil) ::
- *         mkAddToListSection(
- *         List(
- *           mkFormComponent("a", Text(BasicText, Value)),
- *           mkFormComponent("b", Text(BasicText, Value)),
- *           mkFormComponent("c", Text(BasicText, Value))
- *         ),
- *         List(
- *           mkFormComponent("d", Text(BasicText, Value)),
- *           mkFormComponent("e", Text(BasicText, Value))
- *         )
- *       ) :: Nil
- *
- *     val formModel: FormModel[FullyExpanded] = mkFormModel(sections, data)
- *     //val expectedFormModel: FormModel[FullyExpanded] = mkFormModel(sections, expectedData)
- *     //formModel.pages.foreach(println)
- *     val visitsIndex: VisitIndex = VisitIndex(Set.empty[Int])
- *     val expectedVisitsIndex: VisitIndex = VisitIndex(Set.empty[Int]) // TODO JoVl visitindex needs to be updated
- *
- *     val obligations: Obligations = NotChecked
- *     val expectedObligations: Obligations = NotChecked
- *     val processData: ProcessData = ProcessData(data, formModel, visitsIndex, obligations)
- *
- *     //val idx: Int = 1
- *     val addToListId: AddToListId = AddToListId("owner")
- *
- *     /\* val res = AddToListUtils.removeRecord(processData, idx, addToListId)
- *      *
- *      * println("res: ")
- *      * res._1.data.foreach(println)
- *      * println("expectedData.data: ")
- *      * expectedData.data.data.foreach(println)
- *      *
- *      * res._1 shouldBe expectedData.data
- *      * res._2 shouldBe expectedVisitsIndex *\/
- *
- *     val variations = Table(
- *       ("index", "expected"),
- *       (1, expectedData1),
- *       (2, expectedData2),
- *       (3, expectedData3)
- *     )
- *
- *     forAll(variations) { (index, expected) ⇒
- *       val res = AddToListUtils.removeRecord(processData, index, addToListId)
- *
- *       res shouldBe expected.data
- *     }
- *
- *     /\* inside(res) {
- *    *   case ProcessData(data, formModel, visitsIndex, obligations) =>
- *    *     data shouldBe expectedData
- *    *     formModel shouldBe expectedFormModel
- *    *     visitsIndex shouldBe expectedVisitsIndex
- *    *     obligations shouldBe expectedObligations
- *    * } *\/
- *   }
- */
+import org.scalatest.{ FlatSpec, Matchers }
+import org.scalatest.prop.TableDrivenPropertyChecks.{ Table, forAll }
+import uk.gov.hmrc.gform.graph.FormTemplateBuilder._
+import uk.gov.hmrc.gform.models.optics.DataOrigin
+import uk.gov.hmrc.gform.sharedmodel.VariadicValue.{ Many, One }
+import uk.gov.hmrc.gform.sharedmodel.form.FormModelOptics
+import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ AddToListId, FormComponentId, Section, ShortText, Text, Value }
+import uk.gov.hmrc.gform.sharedmodel.{ SourceOrigin, VariadicFormData }
+
+class AddToListUtilsSpec extends FlatSpec with Matchers with FormModelSupport with VariadicFormDataSupport {
+  "AddToListUtils.removeRecord" should "remove instance by its index" in {
+    val data: VariadicFormData[SourceOrigin.OutOfDate] = mkVariadicFormData(
+      "regular" -> One("r"),
+      // Owner data
+      "1_a"     -> One("a_1"),
+      "1_b"     -> One("b_1"),
+      "1_c"     -> One("c_1"),
+      "1_d"     -> One("d_1"),
+      "1_e"     -> One("e_1"),
+      "2_a"     -> One("a_2"),
+      "2_b"     -> One("b_2"),
+      "2_c"     -> One("c_2"),
+      "2_d"     -> One("d_2"),
+      "2_e"     -> One("e_2"),
+      "3_a"     -> One("a_3"),
+      "3_b"     -> One("b_3"),
+      "3_c"     -> One("c_3"),
+      "3_d"     -> One("d_3"),
+      "3_e"     -> One("e_3"),
+      "1_owner" -> Many("0" :: Nil),
+      "2_owner" -> Many("0" :: Nil),
+      "3_owner" -> Many("1" :: Nil),
+      // Fruit data
+      "1_fruitA" -> One("apple_1"),
+      "1_fruitB" -> One("banana_1"),
+      "2_fruitA" -> One("apple_2"),
+      "2_fruitB" -> One("banana_2"),
+      "1_fruit"  -> Many("0" :: Nil)
+    )
+
+    val expectedData1: VariadicFormData[SourceOrigin.OutOfDate] = mkVariadicFormData(
+      "regular"  -> One("r"),
+      "1_a"      -> One("a_2"),
+      "1_b"      -> One("b_2"),
+      "1_c"      -> One("c_2"),
+      "1_d"      -> One("d_2"),
+      "1_e"      -> One("e_2"),
+      "2_a"      -> One("a_3"),
+      "2_b"      -> One("b_3"),
+      "2_c"      -> One("c_3"),
+      "2_d"      -> One("d_3"),
+      "2_e"      -> One("e_3"),
+      "1_owner"  -> Many("0" :: Nil),
+      "1_fruitA" -> One("apple_1"),
+      "1_fruitB" -> One("banana_1"),
+      "2_fruitA" -> One("apple_2"),
+      "2_fruitB" -> One("banana_2"),
+      "1_fruit"  -> Many("0" :: Nil)
+    )
+
+    val expectedData2: VariadicFormData[SourceOrigin.OutOfDate] = mkVariadicFormData(
+      "regular"  -> One("r"),
+      "1_a"      -> One("a_1"),
+      "1_b"      -> One("b_1"),
+      "1_c"      -> One("c_1"),
+      "1_d"      -> One("d_1"),
+      "1_e"      -> One("e_1"),
+      "2_a"      -> One("a_3"),
+      "2_b"      -> One("b_3"),
+      "2_c"      -> One("c_3"),
+      "2_d"      -> One("d_3"),
+      "2_e"      -> One("e_3"),
+      "1_owner"  -> Many("0" :: Nil),
+      "1_fruitA" -> One("apple_1"),
+      "1_fruitB" -> One("banana_1"),
+      "2_fruitA" -> One("apple_2"),
+      "2_fruitB" -> One("banana_2"),
+      "1_fruit"  -> Many("0" :: Nil)
+    )
+
+    val expectedData3: VariadicFormData[SourceOrigin.OutOfDate] = mkVariadicFormData(
+      "regular"  -> One("r"),
+      "1_a"      -> One("a_1"),
+      "1_b"      -> One("b_1"),
+      "1_c"      -> One("c_1"),
+      "1_d"      -> One("d_1"),
+      "1_e"      -> One("e_1"),
+      "2_a"      -> One("a_2"),
+      "2_b"      -> One("b_2"),
+      "2_c"      -> One("c_2"),
+      "2_d"      -> One("d_2"),
+      "2_e"      -> One("e_2"),
+      "1_owner"  -> Many("0" :: Nil),
+      "1_fruitA" -> One("apple_1"),
+      "1_fruitB" -> One("banana_1"),
+      "2_fruitA" -> One("apple_2"),
+      "2_fruitB" -> One("banana_2"),
+      "1_fruit"  -> Many("0" :: Nil)
+    )
+
+    val expectedData4: VariadicFormData[SourceOrigin.OutOfDate] = mkVariadicFormData(
+      "regular"  -> One("r"),
+      "1_a"      -> One("a_1"),
+      "1_b"      -> One("b_1"),
+      "1_c"      -> One("c_1"),
+      "1_d"      -> One("d_1"),
+      "1_e"      -> One("e_1"),
+      "2_a"      -> One("a_2"),
+      "2_b"      -> One("b_2"),
+      "2_c"      -> One("c_2"),
+      "2_d"      -> One("d_2"),
+      "2_e"      -> One("e_2"),
+      "3_a"      -> One("a_3"),
+      "3_b"      -> One("b_3"),
+      "3_c"      -> One("c_3"),
+      "3_d"      -> One("d_3"),
+      "3_e"      -> One("e_3"),
+      "1_owner"  -> Many("0" :: Nil),
+      "2_owner"  -> Many("0" :: Nil),
+      "3_owner"  -> Many("1" :: Nil),
+      "1_fruitA" -> One("apple_2"),
+      "1_fruitB" -> One("banana_2")
+    )
+
+    val expectedData5: VariadicFormData[SourceOrigin.OutOfDate] = mkVariadicFormData(
+      "regular"  -> One("r"),
+      "1_a"      -> One("a_1"),
+      "1_b"      -> One("b_1"),
+      "1_c"      -> One("c_1"),
+      "1_d"      -> One("d_1"),
+      "1_e"      -> One("e_1"),
+      "2_a"      -> One("a_2"),
+      "2_b"      -> One("b_2"),
+      "2_c"      -> One("c_2"),
+      "2_d"      -> One("d_2"),
+      "2_e"      -> One("e_2"),
+      "3_a"      -> One("a_3"),
+      "3_b"      -> One("b_3"),
+      "3_c"      -> One("c_3"),
+      "3_d"      -> One("d_3"),
+      "3_e"      -> One("e_3"),
+      "1_owner"  -> Many("0" :: Nil),
+      "2_owner"  -> Many("0" :: Nil),
+      "3_owner"  -> Many("1" :: Nil),
+      "1_fruitA" -> One("apple_1"),
+      "1_fruitB" -> One("banana_1")
+    )
+
+    val sections: List[Section] =
+      mkSection(mkFormComponent("regular", Text(ShortText.default, Value)) :: Nil) ::
+        mkAddToListSection(
+        "owner",
+        List(
+          mkFormComponent("a", Text(ShortText.default, Value)),
+          mkFormComponent("b", Text(ShortText.default, Value)),
+          mkFormComponent("c", Text(ShortText.default, Value))
+        ),
+        List(
+          mkFormComponent("d", Text(ShortText.default, Value)),
+          mkFormComponent("e", Text(ShortText.default, Value))
+        )
+      ) :: mkAddToListSection(
+        "fruit",
+        List(
+          mkFormComponent("fruitA", Text(ShortText.default, Value))
+        ),
+        List(
+          mkFormComponent("fruitB", Text(ShortText.default, Value))
+        )
+      ) :: Nil
+
+    val formModelOptics: FormModelOptics[DataOrigin.Browser] = mkFormModelOptics(mkFormTemplate(sections), data)
+
+    val processData: ProcessData = mkProcessData(formModelOptics)
+
+    val ownerAddToListId: AddToListId = AddToListId(FormComponentId("owner"))
+    val fruitAddToListId: AddToListId = AddToListId(FormComponentId("fruit"))
+
+    val variations = Table(
+      ("index", "addToListId", "expected"),
+      (0, ownerAddToListId, expectedData1),
+      (1, ownerAddToListId, expectedData2),
+      (2, ownerAddToListId, expectedData3),
+      (0, fruitAddToListId, expectedData4),
+      (1, fruitAddToListId, expectedData5)
+    )
+
+    forAll(variations) { (index, addToListId, expected) ⇒
+      val bracket: Bracket.AddToList[DataExpanded] =
+        formModelOptics.formModelRenderPageOptics.formModel.brackets.addToListBracket(addToListId)
+      val res = AddToListUtils.removeRecord(processData, bracket, index)
+
+      res shouldBe expected
+    }
+  }
 }
