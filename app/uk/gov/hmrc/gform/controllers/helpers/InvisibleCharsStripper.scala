e55924a6e13518a6e062a8b49d9a17998da7963a
girishkamat <girish.kamat@gmail.com>
2020-12-14 13:51:20 +0000

    GFORMS-949 Strip invisible characters from posted data

--- app/uk/gov/hmrc/gform/controllers/helpers/InvisibleCharsStripper.scala
+++ app/uk/gov/hmrc/gform/controllers/helpers/InvisibleCharsStripper.scala
@@ -18,29 +18,8 @@ package uk.gov.hmrc.gform.controllers.helpers
 
 object InvisibleCharsStripper {
 
-  /**
-    * U+2000    En Quad                &#8192;      " "
-    * U+2001    Em Quad                &#8193;      " "
-    * U+2002    En Space               &#8194;      " "
-    * U+2003    Em Space               &#8195;      " "
-    * U+2004    Three-Per-Em Space     &#8196;      " "
-    * U+2005    Four-Per-Em Space      &#8197;      " "
-    * U+2006    Six-Per-Em Space       &#8198;      " "
-    * U+2007    Figure Space           &#8199;      " "
-    * U+2008    Punctuation Space      &#8200;      " "
-    * U+2009    Thin Space             &#8201;      " "
-    * U+200A    Hair Space             &#8202;      " "
-    * U+200B    Zero-Width Space       &#8203;      "​"
-    * U+200C    Zero Width Non-Joiner  &#8204;      "‌"
-    * U+200D    Zero Width Joiner      &#8205;      "‍"
-    * U+200E    Left-To-Right Mark     &#8206;      "‎"
-    * U+200F    Right-To-Left Mark     &#8207;      "‏"
-    * U+202F    Narrow No-Break Space  &#8239;      " "
-    * U+202C    POP DIRECTIONAL FORMATTING &#8236;
-    * U+FEFF    Byte order mark
-    */
   private val INVISIBLE_CHARS_REGEX =
-    "(\u2000|\u2001|\u2002|\u2003|\u2004|\u2005|\u2006|\u2007|\u2008|\u2009|\u200A|\u200B|\u200C|\u200D|\u200E|\u200F|\u202F|\u202C|\uFEFF)"
+    "(\u061C|\u2000|\u2001|\u2002|\u2003|\u2004|\u2005|\u2006|\u2007|\u2008|\u2009|\u200A|\u200B|\u200C|\u200D|\u200E|\u200F|\u202A|\u202B|\u202C|\u202D|\u202E|\u202F|\u2066|\u2067|\u2068|\u2069|\uFEFF)"
 
   def stripInvisibleChars(input: String) = input.replaceAll(INVISIBLE_CHARS_REGEX, "")
 }
