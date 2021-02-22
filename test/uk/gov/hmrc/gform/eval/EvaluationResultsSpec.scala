60e3e260a563ce26ac720e92bab36e905b1a2c20
Girish Kamath <girishkamat@users.noreply.github.com>
2021-02-22 13:41:41 +0000

    GFORMS-1097 Show AddToList count in text fields (#1081)

--- test/uk/gov/hmrc/gform/eval/EvaluationResultsSpec.scala
+++ test/uk/gov/hmrc/gform/eval/EvaluationResultsSpec.scala
@@ -18,18 +18,18 @@ package uk.gov.hmrc.gform.eval
 
 import org.scalatest.prop.TableDrivenPropertyChecks
 import uk.gov.hmrc.gform.Spec
-import uk.gov.hmrc.gform.eval.ExpressionResult.{ DateResult, StringResult }
+import uk.gov.hmrc.gform.eval.ExpressionResult.{ DateResult, NumberResult, StringResult }
 import uk.gov.hmrc.gform.graph.RecData
 import uk.gov.hmrc.gform.models.ExpandUtils.toModelComponentId
 import uk.gov.hmrc.gform.sharedmodel.SourceOrigin.OutOfDate
-import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ Constant, DateCtx, DateFormCtxVar, Else, FormComponentId, FormCtx }
+import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ Constant, Count, DateCtx, DateFormCtxVar, Else, FormComponentId, FormCtx }
 import uk.gov.hmrc.gform.sharedmodel.{ VariadicFormData, VariadicValue }
 
 import java.time.LocalDate
 
 class EvaluationResultsSpec extends Spec with TableDrivenPropertyChecks {
 
-  "evalExpr" should "evaluate expression of type dateString" in {
+  "evalExpr - type dateString" should "evaluate expressions" in {
 
     val recData = RecData[OutOfDate](
       VariadicFormData.create(
@@ -80,14 +80,7 @@ class EvaluationResultsSpec extends Spec with TableDrivenPropertyChecks {
     }
   }
 
-  it should "evaluate expression of type string" in {
-    val recData = RecData[OutOfDate](
-      VariadicFormData.create(
-        (toModelComponentId("dateFieldId1-year"), VariadicValue.One("1970")),
-        (toModelComponentId("dateFieldId1-month"), VariadicValue.One("1")),
-        (toModelComponentId("dateFieldId1-day"), VariadicValue.One("11")),
-        (toModelComponentId("dateFieldId1"), VariadicValue.One("11 January 1970"))
-      ))
+  "evalExpr - type string" should "evaluate expressions" in {
 
     val table = Table(
       ("typeInfo", "recData", "expectedResult", "scenario"),
@@ -95,14 +88,60 @@ class EvaluationResultsSpec extends Spec with TableDrivenPropertyChecks {
         TypeInfo(
           DateCtx(DateFormCtxVar(FormCtx(FormComponentId("dateFieldId1")))),
           StaticTypeData(ExprType.string, None)),
-        recData,
+        RecData[OutOfDate](
+          VariadicFormData.create(
+            (toModelComponentId("dateFieldId1-year"), VariadicValue.One("1970")),
+            (toModelComponentId("dateFieldId1-month"), VariadicValue.One("1")),
+            (toModelComponentId("dateFieldId1-day"), VariadicValue.One("11")),
+            (toModelComponentId("dateFieldId1"), VariadicValue.One("11 January 1970"))
+          )),
         StringResult("11 January 1970"),
         "DateCtx expression converted to type 'string'"),
       (
         TypeInfo(FormCtx(FormComponentId("dateFieldId1")), StaticTypeData(ExprType.string, None)),
-        recData,
+        RecData[OutOfDate](
+          VariadicFormData.create(
+            (toModelComponentId("dateFieldId1-year"), VariadicValue.One("1970")),
+            (toModelComponentId("dateFieldId1-month"), VariadicValue.One("1")),
+            (toModelComponentId("dateFieldId1-day"), VariadicValue.One("11")),
+            (toModelComponentId("dateFieldId1"), VariadicValue.One("11 January 1970"))
+          )),
         StringResult("11 January 1970"),
-        "FormCtx Expression converted to type 'string'")
+        "FormCtx Expression converted to type 'string'"),
+      (
+        TypeInfo(Count(FormComponentId("addToListQuestion")), StaticTypeData(ExprType.string, None)),
+        RecData[OutOfDate](
+          VariadicFormData.create(
+            (toModelComponentId("1_addToListQuestion"), VariadicValue.One("0")),
+            (toModelComponentId("2_addToListQuestion"), VariadicValue.One("1")),
+            (toModelComponentId("1_addToListField1"), VariadicValue.One("Hello")),
+            (toModelComponentId("2_addToListField1"), VariadicValue.One("World"))
+          )),
+        StringResult("2"),
+        "Eval Count(addToListComponent) as string"
+      )
+    )
+    forAll(table) { (typeInfo: TypeInfo, recData: RecData[OutOfDate], expectedResult: ExpressionResult, _) =>
+      EvaluationResults.empty.evalExpr(typeInfo, recData, evaluationContext) shouldBe expectedResult
+    }
+  }
+
+  "evalExpr - type number" should "evaluate expressions" in {
+    val recData = RecData[OutOfDate](
+      VariadicFormData.create(
+        (toModelComponentId("1_addToListQuestion"), VariadicValue.One("0")),
+        (toModelComponentId("2_addToListQuestion"), VariadicValue.One("1")),
+        (toModelComponentId("1_addToListField1"), VariadicValue.One("Hello")),
+        (toModelComponentId("2_addToListField1"), VariadicValue.One("World"))
+      ))
+
+    val table = Table(
+      ("typeInfo", "recData", "expectedResult", "scenario"),
+      (
+        TypeInfo(Count(FormComponentId("addToListQuestion")), StaticTypeData(ExprType.number, None)),
+        recData,
+        NumberResult(2),
+        "Ref to AddToList count in number field")
     )
     forAll(table) { (typeInfo: TypeInfo, recData: RecData[OutOfDate], expectedResult: ExpressionResult, _) =>
       EvaluationResults.empty.evalExpr(typeInfo, recData, evaluationContext) shouldBe expectedResult
