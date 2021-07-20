3b8386f6dffadfcb9821c7f56aea1da0481fe994
girishkamat <girish.kamat@gmail.com>
2021-07-20 16:36:14 +0100

    performance test

--- app/uk/gov/hmrc/gform/models/FormModel.scala
+++ app/uk/gov/hmrc/gform/models/FormModel.scala
@@ -161,7 +161,7 @@ case class FormModel[A <: PageMode](
     toStaticTypeData(fcId).fold(illegal)(staticTypeData => TypeInfo(expr, staticTypeData))
   }
 
-  def pageLookup: Map[FormComponentId, PageModel[A]] =
+  val pageLookup: Map[FormComponentId, PageModel[A]] =
     pages.foldLeft(Map.empty[FormComponentId, PageModel[A]])(_ ++ _.pageLookup)
 
   def find(modelComponentId: ModelComponentId): Option[FormComponent] =
