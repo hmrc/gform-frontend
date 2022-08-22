a06d703380dc3dfd70780a813e2ea6d499ae1bcf
Alex Olkhovskiy <lxol@users.noreply.github.com>
2022-08-22 10:23:41 +0100

    GFORMS-1858: Enhance the country column lookup expression to check an array

--- app/uk/gov/hmrc/gform/sharedmodel/formtemplate/Expr.scala
+++ app/uk/gov/hmrc/gform/sharedmodel/formtemplate/Expr.scala
@@ -59,6 +59,7 @@ sealed trait Expr extends Product with Serializable {
       case AddressLens(_, _)                       => expr :: Nil
       case DataRetrieveCtx(_, _)                   => expr :: Nil
       case CsvCountryCheck(_, _)                   => expr :: Nil
+      case CsvCountryCountCheck(_, _, _)           => expr :: Nil
       case Size(_, _)                              => expr :: Nil
       case Typed(e, _)                             => expr :: Nil
     }
@@ -100,6 +101,7 @@ sealed trait Expr extends Product with Serializable {
     case AddressLens(formComponentId, _)            => this :: Nil
     case DataRetrieveCtx(_, _)                      => this :: Nil
     case CsvCountryCheck(_, _)                      => this :: Nil
+    case CsvCountryCountCheck(_, _, _)              => this :: Nil
     case Size(_, _)                                 => this :: Nil
     case Typed(expr, _)                             => expr.leafs(formModel)
   }
@@ -130,6 +132,7 @@ sealed trait Expr extends Product with Serializable {
     case AddressLens(_, _)                          => Nil
     case DataRetrieveCtx(id, attribute)             => Nil
     case CsvCountryCheck(_, _)                      => Nil
+    case CsvCountryCountCheck(_, _, _)              => Nil
     case Size(_, _)                                 => Nil
     case Typed(expr, _)                             => expr.sums
   }
@@ -159,6 +162,7 @@ final case class Period(dateCtx1: Expr, dateCtx2: Expr) extends Expr
 final case object LangCtx extends Expr
 final case class DataRetrieveCtx(id: DataRetrieveId, attribute: DataRetrieveAttribute) extends Expr
 final case class CsvCountryCheck(formComponentId: FormComponentId, column: String) extends Expr
+final case class CsvCountryCountCheck(formComponentId: FormComponentId, column: String, value: String) extends Expr
 final case class Size(formComponentId: FormComponentId, index: SizeRefType) extends Expr
 final case class Typed(expr: Expr, tpe: ExplicitExprType) extends Expr
 
