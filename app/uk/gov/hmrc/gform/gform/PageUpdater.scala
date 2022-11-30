2eb8360305633900d4e8a5fa1f12c51899b82e01
Alex Olkhovskiy <lxol@users.noreply.github.com>
2022-11-30 14:14:27 +0000

    GFORMS-1881: Introduce a property to specify which choice redirects on confirmation property
    fix a bug in the ATL includeIf

--- app/uk/gov/hmrc/gform/gform/PageUpdater.scala
+++ app/uk/gov/hmrc/gform/gform/PageUpdater.scala
@@ -34,7 +34,8 @@ class PageUpdater[A <: PageMode](page: Page[A], index: Int, baseIds: List[FormCo
 
   private def expandConfirmation(confirmation: Confirmation) = confirmation.copy(
     question = new FormComponentUpdater(confirmation.question, index, baseIds).updatedWithId,
-    redirects = confirmation.redirects.map(r => r.copy(pageId = r.pageId.withIndex(index)))
+    redirects =
+      confirmation.redirects.map(r => r.copy(pageId = r.pageId.withIndex(index), `if` = expandIncludeIf(r.`if`)))
   )
 
   def updated: Page[A] =
