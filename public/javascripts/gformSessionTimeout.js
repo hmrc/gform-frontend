9995b12f80d257840e6329fbf02eac3d033ea125
Alex Olkhovskiy <lxol@users.noreply.github.com>
2022-12-28 14:31:04 +0000

    enable throttling

--- public/javascripts/gformSessionTimeout.js
+++ public/javascripts/gformSessionTimeout.js
@@ -1,18 +1,18 @@
 (function(global) {
   "use strict";
 
-  var $ = global.jQuery;
-  var GOVUK = global.GOVUK || {};
-  var lang = (global.gform && global.gform.lang) || "en";
-
   function GformSessionTimeout() {
     var self = this;
 
+    var enableBroadcastTimestamp = true;
     function init() {
       $("input").keypress(broadcastTimestamp);
     }
 
     function broadcastTimestamp() {
+      if (!enableBroadcastTimestamp) return;
+      enableBroadcastTimestamp = false; 
+      setTimeout(function () { enableBroadcastTimestamp = true; }, 300000)
       if (window.BroadcastChannel) {
         const channel = new window.BroadcastChannel("session-activity");
         channel.postMessage({
