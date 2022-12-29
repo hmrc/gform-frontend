3e44a15708e17612ccb03b2502a6fec54c3752b0
Alex Olkhovskiy <lxol@users.noreply.github.com>
2022-12-29 08:45:55 +0000

    GFORMS-2013: Update 15 minute timeout counter periodically when user … (#1699)
    * GFORMS-2013: Update 15 minute timeout counter periodically when user is active on the keyboard
    
    * remove logging
    
    * enable throttling
    
    * formatting

---
+++ public/javascripts/gformSessionTimeout.js
@@ -0,0 +1,36 @@
+(function(global) {
+  "use strict";
+
+  function GformSessionTimeout() {
+    var self = this;
+
+    var enableBroadcastTimestamp = true;
+    function init() {
+      $("input").keypress(broadcastTimestamp);
+    }
+
+    function broadcastTimestamp() {
+      if (!enableBroadcastTimestamp) return;
+      enableBroadcastTimestamp = false;
+      setTimeout(function () { enableBroadcastTimestamp = true; }, 300000)
+      if (window.BroadcastChannel) {
+        const channel = new window.BroadcastChannel("session-activity");
+        channel.postMessage({
+          timestamp: Date.now()
+        });
+      }
+      return true;
+    }
+
+    self.GformSessionTimeout = function() {
+      init()
+    }
+  }
+
+  GformSessionTimeout.prototype.init = function() {
+    this.GformSessionTimeout();
+  };
+
+  GOVUK.GformSessionTimeout = GformSessionTimeout;
+  global.GOVUK = GOVUK;
+})(window);
