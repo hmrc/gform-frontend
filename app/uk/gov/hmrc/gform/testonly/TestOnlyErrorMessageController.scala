f8d6d7abf7ec3619a4bf5fd7638203af3d81abb2
Alex Olkhovskiy <lxol@users.noreply.github.com>
2023-07-03 15:51:14 +0100

    refactor TestOnlyErrorMessageController

--- app/uk/gov/hmrc/gform/testonly/TestOnlyErrorMessageController.scala
+++ app/uk/gov/hmrc/gform/testonly/TestOnlyErrorMessageController.scala
@@ -16,7 +16,6 @@
 
 package uk.gov.hmrc.gform.testonly
 
-import play.twirl.api.{ Html, HtmlFormat }
 import play.api.libs.json.{ Json, OFormat }
 
 import uk.gov.hmrc.gform.views.html
@@ -93,7 +92,7 @@ class TestOnlyErrorMessageController(
           if (jsonReport)
             Ok(Json.toJson(englishReport ++ welshReport)).as("application/json")
           else {
-            val table = toHtml(FieldErrorAllReport.makeEnCy(englishReport, welshReport), formTemplateId)
+            val table = toTableCells(EnCyReport.makeEnCy(englishReport, welshReport), formTemplateId)
             val title = if (fullReport) "Full Error Report" else "Error Report"
             Ok(html.debug.errorReport(title, table)).as("text/html")
           }
@@ -167,7 +166,7 @@ class TestOnlyErrorMessageController(
       )
   }
 
-  case class FieldErrorAllReport(
+  case class EnCyReport(
     fieldId: String,
     errorMessageType: String,
     debugInfo: String,
@@ -188,11 +187,11 @@ class TestOnlyErrorMessageController(
     validators: List[(String, String)]
   )
 
-  object FieldErrorAllReport {
+  object EnCyReport {
 
-    implicit val format: OFormat[FieldErrorAllReport] = Json.format[FieldErrorAllReport]
-    def make(reportEn: FieldErrorReport, reportCy: FieldErrorReport): FieldErrorAllReport =
-      new FieldErrorAllReport(
+    implicit val format: OFormat[EnCyReport] = Json.format[EnCyReport]
+    def make(reportEn: FieldErrorReport, reportCy: FieldErrorReport): EnCyReport =
+      new EnCyReport(
         fieldId = reportEn.fieldId,
         errorMessageType = reportEn.errorMessageType,
         debugInfo = reportEn.debugInfo,
@@ -212,109 +211,48 @@ class TestOnlyErrorMessageController(
         messages_Cy = if (reportCy.validators.isEmpty) reportCy.messages else reportEn.validators.map(_._2),
         validators = reportEn.validators
       )
-    def htmlEscape(report: FieldErrorAllReport): FieldErrorAllReport =
-      new FieldErrorAllReport(
-        fieldId = HtmlFormat.escape(report.fieldId).toString,
-        errorMessageType = HtmlFormat.escape(report.errorMessageType).toString,
-        debugInfo = HtmlFormat.escape(report.debugInfo).toString,
-        label_En = HtmlFormat.escape(report.label_En).toString,
-        label_Cy = HtmlFormat.escape(report.label_Cy).toString,
-        shortName_En = HtmlFormat.escape(report.shortName_En).toString,
-        shortName_Cy = HtmlFormat.escape(report.shortName_Cy).toString,
-        errorShortName_En = HtmlFormat.escape(report.errorShortName_En).toString,
-        errorShortName_Cy = HtmlFormat.escape(report.errorShortName_Cy).toString,
-        errorShortNameStart_En = HtmlFormat.escape(report.errorShortNameStart_En).toString,
-        errorShortNameStart_Cy = HtmlFormat.escape(report.errorShortNameStart_Cy).toString,
-        errorExample_En = HtmlFormat.escape(report.errorExample_En).toString,
-        errorExample_Cy = HtmlFormat.escape(report.errorExample_Cy).toString,
-        errorMessage_En = HtmlFormat.escape(report.errorMessage_En).toString,
-        errorMessage_Cy = HtmlFormat.escape(report.errorMessage_Cy).toString,
-        messages_En = report.messages_En.map(m => HtmlFormat.escape(m).toString),
-        messages_Cy = report.messages_Cy.map(HtmlFormat.escape(_).toString),
-        validators = report.validators.map(_.map(HtmlFormat.escape(_).toString))
-      )
-    def makeEnCy(reportEn: List[FieldErrorReport], reportCy: List[FieldErrorReport]): List[FieldErrorAllReport] = {
+    def makeEnCy(reportEn: List[FieldErrorReport], reportCy: List[FieldErrorReport]): List[EnCyReport] = {
       val unionList = (reportEn ++ reportCy).groupBy(_.fieldId).map(_._2.head).toList
 
       unionList.map { element =>
         val en = reportEn.find(_.fieldId == element.fieldId).getOrElse(FieldErrorReport.makeBlank())
         val cy = reportCy.find(_.fieldId == element.fieldId).getOrElse(FieldErrorReport.makeBlank())
-        FieldErrorAllReport.make(en, cy)
+        EnCyReport.make(en, cy)
       }
     }
   }
 
-  def toHtml(report: List[FieldErrorAllReport], formTemplateId: FormTemplateId) = {
-    def reportToRows(report: FieldErrorAllReport): String = {
-      val validatorsLabelAndValues = report.validators.zipWithIndex.flatMap {
+  def toTableCells(enCyReports: List[EnCyReport], formTemplateId: FormTemplateId): List[List[(String, String, String, String)]] = {
+    def reportToRows(enCyReport: EnCyReport): List[(String, String, String, String)] = {
+      val validatorsLabelAndValues = enCyReport.validators.zipWithIndex.flatMap {
         case ((enValidator, cyValidator), index) =>
           List((s"validator $index En", enValidator), (s"validator $index Cy", cyValidator))
       }
       val labelsAndValues = List(
-        ("fieldId", report.fieldId),
-        ("label En", report.label_En),
-        ("label Cy", report.label_Cy),
-        ("shortName En", report.shortName_En),
-        ("shortName Cy", report.shortName_Cy),
-        ("errorShortName En", report.errorShortName_En),
-        ("errorShortName Cy", report.errorShortName_Cy),
-        ("errorShortNameStart En", report.errorShortNameStart_En),
-        ("errorShortNameStart Cy", report.errorShortNameStart_Cy),
-        ("errorExample En", report.errorExample_En),
-        ("errorExample Cy", report.errorExample_Cy),
-        ("errorMessage En", report.errorMessage_En),
-        ("errorMessage Cy", report.errorMessage_Cy)
+        ("fieldId", enCyReport.fieldId),
+        ("label En", enCyReport.label_En),
+        ("label Cy", enCyReport.label_Cy),
+        ("shortName En", enCyReport.shortName_En),
+        ("shortName Cy", enCyReport.shortName_Cy),
+        ("errorShortName En", enCyReport.errorShortName_En),
+        ("errorShortName Cy", enCyReport.errorShortName_Cy),
+        ("errorShortNameStart En", enCyReport.errorShortNameStart_En),
+        ("errorShortNameStart Cy", enCyReport.errorShortNameStart_Cy),
+        ("errorExample En", enCyReport.errorExample_En),
+        ("errorExample Cy", enCyReport.errorExample_Cy),
+        ("errorMessage En", enCyReport.errorMessage_En),
+        ("errorMessage Cy", enCyReport.errorMessage_Cy)
       ) ++ validatorsLabelAndValues
 
-      val messagesCombined = report.messages_En.zipAll(report.messages_Cy, "", "")
+      val messagesCombined = enCyReport.messages_En.zipAll(enCyReport.messages_Cy, "", "")
 
       val combined = labelsAndValues.zipAll(messagesCombined, ("", ""), ("", ""))
 
-      def res(list: List[((String, String), (String, String))]) =
-        list
-          .map { case ((label, value), (enMessage, cyMessage)) =>
-            s"""
-               |<tr class="govuk-table__row">
-               |  <th style="white-space: nowrap; text-align: left;" scope="row" class="govuk-table__header">$label</th>
-               |  <td class="govuk-table__cell" >$value</td>
-               |  <td style="color: #d4351c;" class="govuk-table__cell" >$enMessage</td>
-               |  <td style="color: #d4351c;" class="govuk-table__cell" >$cyMessage</td>
-               |</tr>
-    """.stripMargin
-          }
-          .mkString("")
-
-      res(combined) ++ s"""
-                          |<tr>
-                          |      <th scope="col" style="border:1px solid #b1b4b6;" class="govuk-table__header app-custom-class">Property</th>
-                          |      <th scope="col" style="border:1px solid #b1b4b6;" class="govuk-table__header app-custom-class">Property value</th>
-                          |      <th scope="col" style="border:1px solid #b1b4b6;" class="govuk-table__header app-custom-class">English error messages</th>
-                          |      <th scope="col" style="border:1px solid #b1b4b6;" class="govuk-table__header app-custom-class">Welsh error messages</th>
-                          |</tr>
-    """.stripMargin
+      combined.map { case ((label, value), (enMessage, cyMessage)) =>
+        (label, value, enMessage, cyMessage)
+      }
     }
-
-    val tableRows = report.map(r => FieldErrorAllReport.htmlEscape(r)).flatMap(reportToRows).mkString("")
-    // val tableRows = report.map(r => r).flatMap(reportToRows).mkString("")
-
-    val r = s"""
-               |<table class="govuk-table " >
-               | <thead class="govuk-table__head">
-               |    <tr class="govuk-table__row">
-               |      <th scope="col" style="border:1px solid #b1b4b6; white-space: nowrap;" class="govuk-table__header app-custom-class">Property</th>
-               |      <th scope="col" style="border:1px solid #b1b4b6;" class="govuk-table__header app-custom-class">Property value</th>
-               |      <th scope="col" style="border:1px solid #b1b4b6;" class="govuk-table__header app-custom-class">English error messages</th>
-               |      <th scope="col" style="border:1px solid #b1b4b6;" class="govuk-table__header app-custom-class ">Welsh error messages</th>
-               |    </tr>
-               |  </thead>
-               |<tbody class="govuk-table__body">
-               |  $tableRows
-               |</tbody>
-               |</table>
-""".stripMargin
-
-    Html(r)
-
+    enCyReports.map(reportToRows)
   }
 
   def errorsJsonFull(
