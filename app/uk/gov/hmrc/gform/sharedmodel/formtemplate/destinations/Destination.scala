dd9d79ac646273de8b5319e0cfb3d2f4f93773de
Josef Vlach <38655672+josef-vlach@users.noreply.github.com>
2023-10-03 12:51:34 +0100

    GFORMS-2367 - Convert the version from a string to a number (#1964)

--- app/uk/gov/hmrc/gform/sharedmodel/formtemplate/destinations/Destination.scala
+++ app/uk/gov/hmrc/gform/sharedmodel/formtemplate/destinations/Destination.scala
@@ -74,7 +74,7 @@ object Destination {
     includeIf: DestinationIncludeIf,
     failOnError: Boolean,
     formId: FormId,
-    version: FormTemplateVersion,
+    version: String,
     taxpayerId: Expr,
     regime: String,
     includeSessionInfo: Boolean
@@ -205,7 +205,7 @@ case class UploadableDataStoreDestination(
   includeIf: DestinationIncludeIf,
   failOnError: Option[Boolean],
   formId: FormId,
-  version: FormTemplateVersion,
+  version: String,
   taxpayerId: TextExpression,
   regime: String,
   includeSessionInfo: Option[Boolean]
@@ -228,7 +228,6 @@ case class UploadableDataStoreDestination(
 object UploadableDataStoreDestination {
   implicit val reads: Reads[Destination.DataStore] = new Reads[Destination.DataStore] {
     implicit val formIdFormat: Format[FormId] = FormId.destformat
-    implicit val formTemplateVersionFormat: Format[FormTemplateVersion] = FormTemplateVersion.destformat
     private val d: Reads[UploadableDataStoreDestination] = derived.reads[UploadableDataStoreDestination]()
     override def reads(json: JsValue): JsResult[Destination.DataStore] =
       d.reads(json).flatMap(_.toDataStoreDestination.fold(JsError(_), JsSuccess(_)))
