f8ca9d8b87a64cb5721bc5180317f009b2bdd521
Alex Olkhovskiy <lxol@users.noreply.github.com>
2024-02-12 13:50:43 +0000

    GFORMS-2487: fix an issue with composite auth config

--- app/uk/gov/hmrc/gform/testonly/TestOnlyController.scala
+++ app/uk/gov/hmrc/gform/testonly/TestOnlyController.scala
@@ -528,18 +528,33 @@ class TestOnlyController(
   }
 
   def restoreAll(formTemplateId: FormTemplateId, maybeAccessCode: Option[AccessCode]) =
+    controllerComponents.actionBuilder.async { implicit request =>
+      val snapshotId = request.body.asFormUrlEncoded.get("snapshotId").head
+      // composite auth config redirects to the original url
+      // so we can't use authWithoutRetrievingForm in a POST request here
+      Future.successful(
+        Redirect(
+          uk.gov.hmrc.gform.testonly.routes.TestOnlyController.restoreAllGet(
+            formTemplateId,
+            maybeAccessCode,
+            SnapshotId(snapshotId)
+          )
+        )
+      )
+    }
+
+  def restoreAllGet(formTemplateId: FormTemplateId, maybeAccessCode: Option[AccessCode], snapshotId: SnapshotId) =
     auth.authWithoutRetrievingForm(formTemplateId, OperationWithoutForm.ShowAccessCode) {
       implicit request => implicit lang => cache =>
-        val snapshotId = request.body.asFormUrlEncoded.get("snapshotId").head
         val formTemplateContext = request.attrs(FormTemplateKey)
         for {
           // create a brand new form with snapshot's template id
-          _ <- gformConnector.restoreSnapshotTemplate(snapshotId)
+          _ <- gformConnector.restoreSnapshotTemplate(snapshotId.value)
           _ <- newFormController.continue(cache, formTemplateContext.formTemplate)
         } yield Redirect(
           uk.gov.hmrc.gform.testonly.routes.TestOnlyController.restoreContinue(
             formTemplateId,
-            snapshotId,
+            snapshotId.value,
             maybeAccessCode
           )
         )
