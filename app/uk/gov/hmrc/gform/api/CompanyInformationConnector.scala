1964133b95d8b9fa6c75a311ff72d77f1729af41
Aashvin <55795472+Aashvin@users.noreply.github.com>
2024-05-21 10:26:17 +0100

    Adds custom connector method to CompanyInformationConnector

--- app/uk/gov/hmrc/gform/api/CompanyInformationConnector.scala
+++ app/uk/gov/hmrc/gform/api/CompanyInformationConnector.scala
@@ -16,10 +16,12 @@
 
 package uk.gov.hmrc.gform.api
 
-import uk.gov.hmrc.gform.gform.DataRetrieveConnectorBlueprint
+import org.slf4j.{ Logger, LoggerFactory }
+import play.api.http.Status
 import uk.gov.hmrc.gform.sharedmodel.{ CannotRetrieveResponse, DataRetrieve, ServiceCallResponse, ServiceResponse }
 import uk.gov.hmrc.gform.wshttp.WSHttp
-import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.{ HeaderCarrier, HttpResponse }
+import uk.gov.hmrc.http.HttpReads.Implicits.readRaw
 
 import scala.concurrent.{ ExecutionContext, Future }
 
@@ -34,15 +36,45 @@ class CompanyInformationAsyncConnector(ws: WSHttp, baseUrl: String)(implicit ex:
     extends CompanyInformationConnector[Future] {
 
   private val urlWithPlaceholders = s"$baseUrl/companies-house-api-proxy/company/{{companyNumber}}"
-  private val companyProfileB = new DataRetrieveConnectorBlueprint(ws, urlWithPlaceholders, "company profile")
+
+  private val logger: Logger = LoggerFactory.getLogger(getClass)
+  private val identifier = "company profile"
 
   override def companyProfile(
     dataRetrieve: DataRetrieve,
     request: DataRetrieve.Request
-  )(implicit hc: HeaderCarrier): Future[ServiceCallResponse[DataRetrieve.Response]] =
-    companyProfileB.get(dataRetrieve, request).map {
-      case CannotRetrieveResponse => ServiceResponse[DataRetrieve.Response](DataRetrieve.Response.Object(Map.empty))
-      case otherwise              => otherwise
-    }
+  )(implicit hc: HeaderCarrier): Future[ServiceCallResponse[DataRetrieve.Response]] = {
+    val url = request.fillPlaceholders(urlWithPlaceholders)
 
+    ws.GET[HttpResponse](url)
+      .map { httpResponse =>
+        httpResponse.status match {
+          case Status.OK =>
+            dataRetrieve
+              .processResponse(httpResponse.json)
+              .fold(
+                invalid => {
+                  logger.error(
+                    s"Calling $identifier returned successfully, but marshalling of data failed with: $invalid"
+                  )
+                  CannotRetrieveResponse
+                },
+                valid => {
+                  logger.info(s"Calling $identifier returned Success.")
+                  ServiceResponse(valid)
+                }
+              )
+          case Status.NOT_FOUND =>
+            logger.info(s"Calling $identifier returned successfully, but no company was found: $httpResponse")
+            ServiceResponse[DataRetrieve.Response](DataRetrieve.Response.Object(Map.empty))
+          case other =>
+            logger.error(s"Problem when calling $identifier. Http status: $other, body: ${httpResponse.body}")
+            CannotRetrieveResponse
+        }
+      }
+      .recover { case ex =>
+        logger.error(s"Unknown problem when calling $identifier", ex)
+        CannotRetrieveResponse
+      }
+  }
 }
