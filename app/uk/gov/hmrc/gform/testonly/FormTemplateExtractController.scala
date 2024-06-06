4ee7d1baa8bcac449a7bb9ebc55d0a1a4358899a
Dursun Gundogan <78355528+DursunGundogan@users.noreply.github.com>
2024-06-06 16:27:06 +0100

    GFORMS-2174 - Produce a report for a form to include the data items and properties of those data items

--- app/uk/gov/hmrc/gform/testonly/FormTemplateExtractController.scala
+++ app/uk/gov/hmrc/gform/testonly/FormTemplateExtractController.scala
@@ -17,20 +17,24 @@
 package uk.gov.hmrc.gform.testonly
 
 import cats.implicits._
+import org.apache.poi.ss.usermodel.{ Cell, Row, Sheet, Workbook }
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import play.api.mvc.{ Action, AnyContent, MessagesControllerComponents }
 import uk.gov.hmrc.gform.auth.models.OperationWithForm
 import uk.gov.hmrc.gform.controllers.{ AuthCacheWithForm, AuthenticatedRequestActions }
 import uk.gov.hmrc.gform.models.SectionSelectorType
-import uk.gov.hmrc.gform.sharedmodel.{ AccessCode, SmartString }
+import uk.gov.hmrc.gform.sharedmodel.{ AccessCode, LangADT, SmartString }
 import uk.gov.hmrc.gform.sharedmodel.formtemplate.{ FormComponent, FormTemplateId, IsInformationMessage, IsMiniSummaryList, IsTableComp }
-import uk.gov.hmrc.gform.testonly.extract.{ FormTemplateDetail, FormTemplateDetailRow }
+import uk.gov.hmrc.gform.testonly.extract.{ FormTemplateDetail, FormTemplateDetailRow, ReportTableRow }
 import uk.gov.hmrc.gform.views.html
 import uk.gov.hmrc.govukfrontend.views.Aliases.{ HeadCell, Table, Text }
 import uk.gov.hmrc.govukfrontend.views.html.components.GovukTable
 import uk.gov.hmrc.govukfrontend.views.viewmodels.table.TableRow
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
+import java.io.ByteArrayOutputStream
 import java.text.MessageFormat
+import scala.collection.immutable.List
 import scala.concurrent.{ ExecutionContext, Future }
 
 class FormTemplateExtractController(
@@ -46,136 +50,50 @@ class FormTemplateExtractController(
     case _                       => false
   }
 
-  def extract(formTemplateId: FormTemplateId, accessCode: Option[AccessCode]): Action[AnyContent] = {
-    auth.authAndRetrieveForm[SectionSelectorType.Normal](formTemplateId, accessCode, OperationWithForm.EditForm) {
-      implicit request => implicit lang => cache => _ => _ =>
-        val formTemplateDetails: List[FormTemplateDetail] = extractFormTemplate(cache)
+  private val headers = List(
+    "Section",
+    "Task",
+    "Details",
+    "Page title",
+    "Page condition",
+    "Field ID",
+    "Field label",
+    "Field format",
+    "Field condition",
+    "Field repeats"
+  )
 
-        val header: List[HeadCell] = List(
-          HeadCell(
-            content = Text("Section")
-          ),
-          HeadCell(
-            content = Text("Task")
-          ),
-          HeadCell(
-            content = Text("Details")
-          ),
-          HeadCell(
-            content = Text("Page title")
-          ),
-          HeadCell(
-            content = Text("Page condition")
-          ),
-          HeadCell(
-            content = Text("Field ID")
-          ),
-          HeadCell(
-            content = Text("Field label")
-          ),
-          HeadCell(
-            content = Text("Field format")
-          ),
-          HeadCell(
-            content = Text("Field condition")
-          ),
-          HeadCell(
-            content = Text("Field repeats")
-          )
-        )
+  private def evalSmartString(smartString: SmartString)(implicit lang: LangADT) =
+    new MessageFormat(smartString.rawDefaultValue)
+      .format(
+        smartString
+          .interpolations(_ => false)
+          .map(i => s"$${${i.prettyPrint}}")
+          .toArray
+      )
 
-        def evalSmartString(smartString: SmartString) =
-          new MessageFormat(smartString.rawDefaultValue)
-            .format(
-              smartString
-                .interpolations(_ => false)
-                .map(i => s"$${${i.prettyPrint}}")
-                .toArray
-            )
+  private def normaliseTableRows(input: List[List[Option[ReportTableRow]]]): List[List[Option[ReportTableRow]]] = {
+    val colCount = input.head.length
+    val rowspans = Array.fill(colCount)(0)
 
-        val formTemplateTableRows: List[List[Some[TableRow]]] = formTemplateDetails.map { row =>
-          List(
-            Some(
-              TableRow(
-                content = Text(evalSmartString(row.sectionTitle.value)),
-                rowspan = Some(row.sectionTitle.rowSpan)
-              )
-            ),
-            Some(
-              TableRow(
-                content = Text(evalSmartString(row.taskTitle.value)),
-                rowspan = Some(row.taskTitle.rowSpan)
-              )
-            ),
-            Some(
-              TableRow(
-                content = Text(row.shortName.map(evalSmartString).getOrElse("")),
-                rowspan = Some(row.pageTitle.rowSpan)
-              )
-            ),
-            Some(
-              TableRow(
-                content = Text(evalSmartString(row.pageTitle.value)),
-                rowspan = Some(row.pageTitle.rowSpan)
-              )
-            ),
-            Some(
-              TableRow(
-                content = Text(row.pageCondition.map(cond => s"$${${cond.booleanExpr.prettyPrint}}").getOrElse("")),
-                rowspan = Some(row.pageTitle.rowSpan)
-              )
-            ),
-            Some(TableRow(content = Text(row.fieldId.value))),
-            Some(TableRow(content = Text(evalSmartString(row.fieldLabel)))),
-            Some(TableRow(content = Text(row.fieldFormat.showType))),
-            Some(
-              TableRow(content =
-                Text(row.fieldCondition.map(cond => s"$${${cond.booleanExpr.prettyPrint}}").getOrElse(""))
-              )
-            ),
-            Some(TableRow(content = Text(if (row.fieldRepeats) "Y" else "N")))
-          )
+    def processRow(row: List[Option[ReportTableRow]]): List[Option[ReportTableRow]] =
+      row.zipWithIndex.map { case (cell, colIndex) =>
+        if (rowspans(colIndex) > 0) {
+          rowspans(colIndex) -= 1
+          None
+        } else {
+          cell match {
+            case Some(ReportTableRow(_, Some(rs))) if rs > 1 =>
+              rowspans(colIndex) = rs - 1
+            case _ =>
+          }
+          cell
         }
+      }
 
-        def normaliseTableRows(input: List[List[Option[TableRow]]]): List[List[Option[TableRow]]] = {
-          val colCount = input.head.length
-          val rowspans = Array.fill(colCount)(0)
-
-          def processRow(row: List[Option[TableRow]]): List[Option[TableRow]] =
-            row.zipWithIndex.map { case (cell, colIndex) =>
-              if (rowspans(colIndex) > 0) {
-                rowspans(colIndex) -= 1
-                None
-              } else {
-                cell match {
-                  case Some(TableRow(_, _, _, _, Some(rs), _)) if rs > 1 =>
-                    rowspans(colIndex) = rs - 1
-                  case _ =>
-                }
-                cell
-              }
-            }
-
-          input.map(processRow)
-        }
-
-        val tableRows = normaliseTableRows(formTemplateTableRows)
-
-        val title = s"Detailed Report for Form Template `${formTemplateId.value}`"
-
-        val table = Table(
-          head = Some(header),
-          rows = tableRows.map(_.flatten),
-          attributes = Map("border" -> "1px solid black"),
-          caption = Some(title),
-          captionClasses = "govuk-table__caption--m"
-        )
-
-        val htmlTable = new GovukTable()(table)
-        Ok(html.debug.formTemplateExtract(title, htmlTable)).pure[Future]
-    }
-
+    input.map(processRow)
   }
+
   private def extractFormTemplate(cache: AuthCacheWithForm) = {
     cache.formTemplate.formKind
       .fold { classic =>
@@ -328,4 +246,120 @@ class FormTemplateExtractController(
         }
       }
   }
+
+  private def makeReportTableRows(cache: AuthCacheWithForm)(implicit lang: LangADT) = {
+    val formTemplateDetails = extractFormTemplate(cache)
+
+    val tableRows = formTemplateDetails.map { row =>
+      List(
+        Some(
+          ReportTableRow(
+            content = evalSmartString(row.sectionTitle.value),
+            rowspan = Some(row.sectionTitle.rowSpan)
+          )
+        ),
+        Some(
+          ReportTableRow(
+            content = evalSmartString(row.taskTitle.value),
+            rowspan = Some(row.taskTitle.rowSpan)
+          )
+        ),
+        Some(
+          ReportTableRow(
+            content = row.shortName.map(evalSmartString).getOrElse(""),
+            rowspan = Some(row.pageTitle.rowSpan)
+          )
+        ),
+        Some(
+          ReportTableRow(
+            content = evalSmartString(row.pageTitle.value),
+            rowspan = Some(row.pageTitle.rowSpan)
+          )
+        ),
+        Some(
+          ReportTableRow(
+            content = row.pageCondition.map(cond => s"$${${cond.booleanExpr.prettyPrint}}").getOrElse(""),
+            rowspan = Some(row.pageTitle.rowSpan)
+          )
+        ),
+        Some(ReportTableRow(content = row.fieldId.value)),
+        Some(ReportTableRow(content = evalSmartString(row.fieldLabel))),
+        Some(ReportTableRow(content = row.fieldFormat.showType)),
+        Some(
+          ReportTableRow(content = row.fieldCondition.map(cond => s"$${${cond.booleanExpr.prettyPrint}}").getOrElse(""))
+        ),
+        Some(ReportTableRow(content = if (row.fieldRepeats) "Y" else "N"))
+      )
+    }
+
+    normaliseTableRows(tableRows)
+  }
+
+  def extract(formTemplateId: FormTemplateId, accessCode: Option[AccessCode]): Action[AnyContent] =
+    auth.authAndRetrieveForm[SectionSelectorType.Normal](formTemplateId, accessCode, OperationWithForm.EditForm) {
+      implicit request => implicit lang => cache => _ => _ =>
+        val header: List[HeadCell] = headers.map(header =>
+          HeadCell(
+            content = Text(header)
+          )
+        )
+
+        val tableRows: List[List[Option[TableRow]]] =
+          makeReportTableRows(cache).map(
+            _.map(_.map(row => TableRow(content = Text(row.content), rowspan = row.rowspan)))
+          )
+
+        val title = s"Detailed Report for Form Template `${formTemplateId.value}`"
+
+        val table = Table(
+          head = Some(header),
+          rows = tableRows.map(_.flatten),
+          attributes = Map("border" -> "1px solid black"),
+          caption = Some(title),
+          captionClasses = "govuk-table__caption--m"
+        )
+
+        val htmlTable = new GovukTable()(table)
+        Ok(html.debug.formTemplateExtract(cache.formTemplate, accessCode, title, htmlTable)).pure[Future]
+    }
+
+  def exportToExcel(formTemplateId: FormTemplateId, accessCode: Option[AccessCode]): Action[AnyContent] =
+    auth.authAndRetrieveForm[SectionSelectorType.Normal](formTemplateId, accessCode, OperationWithForm.EditForm) {
+      _ => implicit lang => cache => _ => _ =>
+        val workbook: Workbook = new XSSFWorkbook()
+        val sheet: Sheet = workbook.createSheet(s"Detailed Report for ${formTemplateId.value}")
+        val headerRow: Row = sheet.createRow(0)
+
+        for (i <- headers.indices) {
+          val cell: Cell = headerRow.createCell(i)
+          cell.setCellValue(headers(i))
+        }
+
+        val tableRows = makeReportTableRows(cache).map(_.map(_.map(_.content).getOrElse("")))
+
+        for (i <- tableRows.indices) {
+          val row: Row = sheet.createRow(i + 1) // Start from the second row
+          for (j <- tableRows(i).indices) {
+            val cell: Cell = row.createCell(j)
+            cell.setCellValue(tableRows(i)(j))
+          }
+        }
+
+        for (i <- headers.indices)
+          sheet.autoSizeColumn(i)
+
+        val outputStream = new ByteArrayOutputStream()
+        workbook.write(outputStream)
+        val fileContents = outputStream.toByteArray
+
+        outputStream.close()
+        workbook.close()
+
+        Ok(fileContents)
+          .as("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+          .withHeaders(
+            "Content-Disposition" -> s"attachment; filename=${formTemplateId.value}-details.xlsx"
+          )
+          .pure[Future]
+    }
 }
