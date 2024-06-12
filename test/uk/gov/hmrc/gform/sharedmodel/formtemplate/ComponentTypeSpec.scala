d1065d5c7e7b4d6323c40d7841cb4f80119e3a52
Aashvin <55795472+Aashvin@users.noreply.github.com>
2024-06-12 10:01:01 +0100

    Adds new tests for time type

--- test/uk/gov/hmrc/gform/sharedmodel/formtemplate/ComponentTypeSpec.scala
+++ test/uk/gov/hmrc/gform/sharedmodel/formtemplate/ComponentTypeSpec.scala
@@ -19,10 +19,44 @@ package uk.gov.hmrc.gform.sharedmodel.formtemplate
 import uk.gov.hmrc.gform.Spec
 import uk.gov.hmrc.gform.sharedmodel.formtemplate.generators.ComponentTypeGen
 
+import java.time.LocalTime
+
 class ComponentTypeSpec extends Spec {
   "ComponentType" should "round trip derived JSON" in {
     forAll(ComponentTypeGen.componentTypeGen()) { obj =>
       ComponentType.format.reads(ComponentType.format.writes(obj)) should beJsSuccess(obj)
     }
   }
+
+  "Range" should "format times without a space between the numbers and the am/pm" in {
+    val formattedTime = Range.twelveHoursFormat.format(LocalTime.parse("01:45"))
+
+    formattedTime shouldBe "01:45am"
+  }
+
+  it should "produce a list of time slots without a space between the numbers and the am/pm" in {
+    val startTime = StartTime(LocalTime.parse("00:00"))
+    val endTime = EndTime(LocalTime.parse("23:59"))
+    val range = Range(startTime, endTime)
+    val intervalMins = IntervalMins(15)
+    val time = Time(List(range), intervalMins)
+
+    val formattedTimes = Range.timeSlots(time)
+
+    formattedTimes.count(timeSlot => timeSlot.contains(" ")) shouldBe 0
+  }
+
+  it should "produce a list of time slots with am/pm all lowercase" in {
+    val startTime = StartTime(LocalTime.parse("00:00"))
+    val endTime = EndTime(LocalTime.parse("23:59"))
+    val range = Range(startTime, endTime)
+    val intervalMins = IntervalMins(15)
+    val time = Time(List(range), intervalMins)
+
+    val formattedTimes = Range.timeSlots(time)
+
+    formattedTimes.count(timeSlot => timeSlot.endsWith("am") || timeSlot.endsWith("pm")) shouldBe formattedTimes.length
+    formattedTimes.count(timeSlot => timeSlot.endsWith("AM") || timeSlot.endsWith("PM")) shouldBe 0
+  }
+
 }
