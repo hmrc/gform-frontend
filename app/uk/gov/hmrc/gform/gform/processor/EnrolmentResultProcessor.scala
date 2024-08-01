d261b1d8e7a72dc2448323a9aa109ed6cf316c78
Josef Vlach <38655672+josef-vlach@users.noreply.github.com>
2024-08-01 13:27:05 +0200

    GFORMS-2702 - Add separate response pages for enrolment section

--- app/uk/gov/hmrc/gform/gform/processor/EnrolmentResultProcessor.scala
+++ app/uk/gov/hmrc/gform/gform/processor/EnrolmentResultProcessor.scala
@@ -68,7 +68,7 @@ class EnrolmentResultProcessor(
           enrolmentSection,
           formModelOptics,
           globalErrors,
-          validationResult
+          validationResultFinal
         )
       )
     }
