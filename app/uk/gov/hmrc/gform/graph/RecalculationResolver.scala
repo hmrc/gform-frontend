5d0fe502c235e8a87fa450d09b3c43a676c3e2fb
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2025-06-16 09:22:27 +0100

    Initial on demand include if

--- app/uk/gov/hmrc/gform/graph/RecalculationResolver.scala
+++ app/uk/gov/hmrc/gform/graph/RecalculationResolver.scala
@@ -55,10 +55,6 @@ class RecalculationResolver[F[_]: Applicative](
         .evalExpr(typeInfo2, recData, booleanExprResolver, evaluationContext)
         .applyTypeInfo(typeInfo2)
 
-    println("typeInfo1: " + typeInfo1)
-    println("typeInfo2: " + typeInfo2)
-    println("exprRes1: " + exprRes1)
-    println("exprRes2: " + exprRes2)
     val res: Boolean = f(exprRes1, exprRes2)
     res
   }
