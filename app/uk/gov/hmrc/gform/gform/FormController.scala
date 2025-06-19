25bf10757b254b8933e5a59ed898dafb25a6bb04
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2025-06-19 08:01:59 +0100

    removed unused code

--- app/uk/gov/hmrc/gform/gform/FormController.scala
+++ app/uk/gov/hmrc/gform/gform/FormController.scala
@@ -263,11 +263,6 @@ class FormController(
                       .filter(onDemandIncludeIfFilter)
                       .getOrElse(throw new RuntimeException("can't filter"))
 
-                  val visibleSectionNumbers =
-                    visibleIteration.singletons.filter(s => onDemandIncludeIfFilter(s.singleton)).map { s =>
-                      s.sectionNumber
-                    }
-
                   validateSections(
                     SuppressErrors.No,
                     visibleIteration.allSingletonSectionNumbers: _*
