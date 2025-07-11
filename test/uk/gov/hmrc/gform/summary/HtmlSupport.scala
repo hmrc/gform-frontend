d8583d63d4b13fc5f3ec6dee6df77be03510be48
Carl Manson <506508+cmanson@users.noreply.github.com>
2025-07-11 16:06:13 +0100

    GFORMS-3548 - The spacing between sections / pages on summary / CYA pages should not be applied when cards are used (#2537)

--- test/uk/gov/hmrc/gform/summary/HtmlSupport.scala
+++ test/uk/gov/hmrc/gform/summary/HtmlSupport.scala
@@ -63,7 +63,7 @@ trait HtmlSupport {
       buildSummaryLists(
         document
           .select(
-            "h2[class='govuk-heading-m govuk-!-margin-top-0'],h3[class='govuk-heading-m govuk-!-margin-top-0'],dl[class='govuk-summary-list govuk-!-margin-bottom-8']"
+            "h2[class='govuk-heading-m govuk-!-margin-top-0'],h3[class='govuk-heading-m govuk-!-margin-top-0'],dl[class='govuk-summary-list'],dl[class='govuk-summary-list govuk-!-margin-bottom-8']"
           )
           .asScala
           .toList
