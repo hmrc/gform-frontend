0901856fd6823b66f96df6ece0e777ac8faa0201
Carl Manson <506508+cmanson@users.noreply.github.com>
2026-05-27 16:01:06 +0100

    GFORMS-3689 - Replay the URI used in API calls within destinations/dataRetrieves

--- app/uk/gov/hmrc/gform/testonly/DataRetrieveDescription.scala
+++ app/uk/gov/hmrc/gform/testonly/DataRetrieveDescription.scala
@@ -17,16 +17,55 @@
 package uk.gov.hmrc.gform.testonly
 
 import julienrf.json.derived
-import play.api.libs.json.{ JsObject, OFormat }
+import play.api.libs.json.{ Format, JsObject, OFormat }
+import uk.gov.hmrc.gform.sharedmodel.DataRetrieve
+import uk.gov.hmrc.gform.sharedmodel.formtemplate.ADTFormat
 
 case class DataRetrieveDescription(
   tpe: String,
   exampleJson: JsObject,
   attributeReferences: List[String],
   documentationUrl: Option[String],
-  isArrayResult: Boolean
+  isArrayResult: Boolean,
+  urlDescriptors: List[UrlDescriptor]
 )
 
 object DataRetrieveDescription {
   implicit val format: OFormat[DataRetrieveDescription] = derived.oformat()
+}
+
+sealed trait UrlDestination extends Product with Serializable
+
+object UrlDestination {
+  case object GForm extends UrlDestination
+  case object MDTP extends UrlDestination
+  case object DES extends UrlDestination
+  case object HIP extends UrlDestination
+  case object CompaniesHouse extends UrlDestination
+
+  val companiesHouse = "Companies House"
+
+  implicit val format: Format[UrlDestination] =
+    ADTFormat.formatEnumeration(
+      "GForm"        -> GForm,
+      "MDTP"         -> MDTP,
+      "DES"          -> DES,
+      "HIP"          -> HIP,
+      companiesHouse -> CompaniesHouse
+    )
+
+  def asString(urlDestination: UrlDestination): String = urlDestination match {
+    case CompaniesHouse => companiesHouse
+    case other          => other.toString
+  }
+}
+
+final case class UrlDescriptor(
+  urlPath: String,
+  pathParameters: List[DataRetrieve.Parameter],
+  destination: UrlDestination
+)
+
+object UrlDescriptor {
+  implicit val format: OFormat[UrlDescriptor] = derived.oformat()
 }
