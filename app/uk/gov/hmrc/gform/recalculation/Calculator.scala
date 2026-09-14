c80cd4205763737231998accaf4c59aac3148ec5
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2026-09-14 12:07:24 +0100

    Revert "Fix ATL includeIf boolean evaluation"
    This reverts commit adb57a82569a4f4289d97813d007f44e6d57679f.

--- app/uk/gov/hmrc/gform/recalculation/Calculator.scala
+++ app/uk/gov/hmrc/gform/recalculation/Calculator.scala
@@ -41,16 +41,6 @@ trait Calculator {
   def allModelComponentIds(modelComponentId: ModelComponentId): List[(ModelComponentId, EvaluationStatus)]
 }
 
-private[recalculation] object ScalarBooleanExprEvaluator {
-  def evaluate(booleanExpr: BooleanExpr)(evaluateLeaf: BooleanExpr => Boolean): Boolean =
-    booleanExpr match {
-      case Or(lhs, rhs)  => evaluate(lhs)(evaluateLeaf) || evaluate(rhs)(evaluateLeaf)
-      case And(lhs, rhs) => evaluate(lhs)(evaluateLeaf) && evaluate(rhs)(evaluateLeaf)
-      case Not(expr)     => !evaluate(expr)(evaluateLeaf)
-      case leaf          => evaluateLeaf(leaf)
-    }
-}
-
 final class RealCalculator(
   metadata: Metadata,
   evaluationContext: EvaluationContext,
@@ -66,9 +56,7 @@ final class RealCalculator(
   override def evalBooleanExpr(booleanExpr: BooleanExpr): Boolean = {
     val staticTypeData = metadata.booleanExprStaticType(booleanExpr)
 
-    ScalarBooleanExprEvaluator.evaluate(booleanExpr) { leaf =>
-      evalBooleanExpr(leaf, staticTypeData, Behaviour.Default).exists(_ == true)
-    }
+    evalBooleanExpr(booleanExpr, staticTypeData, Behaviour.Default).exists(_ == true)
   }
 
   override def evalBooleanExprList(booleanExpr: BooleanExpr, behaviour: Behaviour): List[Boolean] = {
